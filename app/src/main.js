7d979025cd312f378a521d617e7eb7e8e33091cb
Daniel Dembach <daniel@dmbch.net>
2016-04-21 00:56:18 +0200

    introduce component factory functions

--- app/src/main.js
+++ app/src/main.js
@@ -1,38 +1,38 @@
 
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { Route } from 'react-router';
-import { connect } from 'react-redux';
 
-import { createAction, createReducer, render } from 'hops';
+import { createContainer, createAction, createReducer, render } from 'hops';
 
 import { headline } from './style.css';
 
 const namespace = 'home';
-@connect(
-  state => state[namespace],
+
+const Home = createContainer(
+  (state) => state[namespace],
   {
     update: createAction(namespace)
+  },
+  {
+    propTypes: {
+      greeting: PropTypes.string,
+      update: PropTypes.func
+    },
+    componentDidMount: function () {
+      const { update } = this.props;
+      update({'greeting': {'$set': 'Hello World!'}});
+    },
+    render: function () {
+      const { greeting } = this.props;
+      return (
+        <h1 className={ headline }>{ greeting }</h1>
+      );
+    }
   }
-)
-class Home extends Component {
-  static propTypes = {
-    greeting: PropTypes.string,
-    update: PropTypes.func
-  };
-  componentDidMount() {
-    const { update } = this.props;
-    update({'greeting': {'$set': 'Hello World!'}});
-  }
-  render() {
-    const { greeting } = this.props;
-    return (
-      <h1 className={ headline }>{ greeting }</h1>
-    );
-  }
-}
+);
 
 const routes = (
-  <Route path='/' component={ Home } />
+  <Route path='/' component={ Home }/>
 );
 const reducers = {[namespace]: createReducer(namespace)};
 
