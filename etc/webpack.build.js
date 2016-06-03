9f2e9092512cb384aed9e9fe93cb3ade32cf7efe
Daniel Dembach <daniel@dmbch.net>
2016-06-03 15:51:49 +0200

    remove hops.js runner, switch to plain webpack

--- etc/webpack.build.js
+++ etc/webpack.build.js
@@ -5,8 +5,29 @@ var WebpackConfig = require('webpack-config').default;
 var baseConfig = require('../lib/config').webpackBase;
 
 module.exports = new WebpackConfig().extend(baseConfig).merge({
+  filename: __filename,
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'eslint',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: 'stylelint',
+        exclude: /node_modules/
+      }
+    ]
+  },
   plugins: [
     new webpack.EnvironmentPlugin(['NODE_ENV']),
-    new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false }})
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(true),
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
+      output: { comments: false },
+      compress: { warnings: false }
+    })
   ]
 });
--- etc/webpack.dev.js
+++ etc/webpack.dev.js
@@ -5,8 +5,14 @@ var config = require('../lib/config');
 var baseConfig = config.webpackBase;
 
 module.exports = new WebpackConfig().extend(baseConfig).merge({
+  filename: __filename,
+  devtool: 'source-map',
+  output: {
+    pathinfo: true
+  },
   devServer: {
     contentBase: config.distDir,
+    noInfo: true,
     watchOptions: {
       aggregateTimeout: 100
     }
