e34503f910d09f8addd55787ca5d9193768515f6
Daniel Dembach <daniel@dmbch.net>
2016-06-23 00:04:06 +0200

    fix test failures, finalize dll generation setup

--- bin/precompile.js
+++ bin/precompile.js
@@ -3,47 +3,17 @@
 var path = require('path');
 
 var webpack = require('webpack');
-var nodeExternals = require('webpack-node-externals');
 
 var helpers = require('../config/helpers');
 
-function getConfig(prefix, overrides) {
-  var defaults = {
-    output: {
-      path: path.resolve(helpers.tmp, prefix),
-      filename: 'vendor.js',
-      library: '__vendor__'
-    },
-    plugins: [
-      new webpack.DllPlugin({
-        context: helpers.root,
-        path: path.resolve(helpers.tmp, prefix, 'vendor-manifest.json'),
-        name: '__vendor__'
-      }),
-      new webpack.ProgressPlugin()
-    ]
-  };
-  return helpers.extend('webpack.dll.js', helpers.merge(defaults, overrides));
-}
-
 if (path.resolve(__dirname, '..') !== helpers.root) {
-  var configs = [
-    getConfig('watch', {
-      devtool: 'cheap-module-eval-source-map'
-    }),
-    getConfig('build', {
-      plugins: [
-        new webpack.DefinePlugin({
-          'process.env.NODE_ENV': '"production"'
-        }),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-          compress: { warnings: false, unused: true, 'dead_code': true }
-        })
-      ]
-    })
-  ];
-  configs.forEach(function (config) {
+  // eslint-disable-next-line no-console
+  console.log('precompile dll:');
+  var defaults = helpers.extend('webpack.dll.js', {});
+  var modes = defaults.modes || {};
+  delete defaults.modes;
+  Object.keys(modes).forEach(function (mode) {
+    var config = helpers.merge(defaults, modes[mode]);
     webpack(config).run(function(error) {
       // eslint-disable-next-line no-console
       if (error) { console.log(error); }
