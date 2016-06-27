2285ba6d2dc76bce4b0eea7db07408aed9980bf0
Daniel Dembach <daniel@dmbch.net>
2016-06-28 00:44:09 +0200

    switch precompile to native webpack multi-compiler mode

--- etc/webpack.dll.js
+++ etc/webpack.dll.js
@@ -5,7 +5,7 @@ var webpack = require('webpack');
 
 var helpers = require('../config/helpers');
 
-module.exports = {
+var defaults = {
   entry: {
     hops: ['hops']
   },
@@ -13,41 +13,41 @@ module.exports = {
     filename: '[name].js',
     library: '__[name]__'
   },
-  modes: {
-    watch: {
-      output: {
-        path: path.resolve(helpers.tmp, 'watch')
-      },
-      devtool: 'cheap-module-eval-source-map',
-      plugins: [
-        new webpack.DllPlugin({
-          context: helpers.root,
-          path: path.resolve(helpers.tmp, 'watch', '[name].json'),
-          name: '__[name]__'
-        })
-      ]
-    },
-    build: {
-      output: {
-        path: path.resolve(helpers.tmp, 'build')
-      },
-      plugins: [
-        new webpack.DllPlugin({
-          context: helpers.root,
-          path: path.resolve(helpers.tmp, 'build', '[name].json'),
-          name: '__[name]__'
-        }),
-        new webpack.DefinePlugin({
-          'process.env.NODE_ENV': '"production"'
-        }),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-          compress: { warnings: false, unused: true, 'dead_code': true },
-          output: { comments: false }
-        })
-      ]
-    }
-  },
-  plugins: [ new webpack.ProgressPlugin() ],
-  extend: helpers.extend.bind(null, __filename)
+  plugins: [ new webpack.ProgressPlugin() ]
 };
+
+module.exports = [
+  helpers.merge(defaults, {
+    output: {
+      path: path.resolve(helpers.tmp, 'watch')
+    },
+    devtool: 'cheap-module-eval-source-map',
+    plugins: [
+      new webpack.DllPlugin({
+        context: helpers.root,
+        path: path.resolve(helpers.tmp, 'watch', '[name].json'),
+        name: '__[name]__'
+      })
+    ]
+  }),
+  helpers.merge(defaults, {
+    output: {
+      path: path.resolve(helpers.tmp, 'build')
+    },
+    plugins: [
+      new webpack.DllPlugin({
+        context: helpers.root,
+        path: path.resolve(helpers.tmp, 'build', '[name].json'),
+        name: '__[name]__'
+      }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': '"production"'
+      }),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: { warnings: false, unused: true, 'dead_code': true },
+        output: { comments: false }
+      })
+    ]
+  })
+];
