823e6904be466b90a4419151757a0ccb24e9c371
Daniel Dembach <daniel@dmbch.net>
2016-08-24 13:58:30 +0200

    fix breaking tests

--- etc/webpack.mock.js
+++ etc/webpack.mock.js
@@ -2,7 +2,7 @@
 
 var path = require('path');
 
-module.exports = require('./webpack.node.js').extend({
+module.exports = require('./webpack.node.js').merge({
   entry: path.resolve(__dirname, '..', '.tmp', 'test'),
   output: {
     path: path.resolve(__dirname, '..', '.tmp', 'test', 'dist')
