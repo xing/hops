80f52b30fe5db921f421f922f2c5b67a9eef5b3c
Daniel Dembach <daniel@dmbch.net>
2017-01-23 13:22:49 +0100

    switch to semistandard

--- middleware/index.js
+++ middleware/index.js
@@ -3,8 +3,7 @@
 var util = require('../lib/util');
 var transpile = require('../transpiler');
 
-
-module.exports = function createMiddleware(hopsConfig, watchOptions) {
+module.exports = function createMiddleware (hopsConfig, watchOptions) {
   var config = util.getConfig(hopsConfig);
 
   var transpilerPromise = null;
@@ -16,27 +15,34 @@ module.exports = function createMiddleware(hopsConfig, watchOptions) {
     middlewarePromise = null;
   });
 
-  function getTranspilerPromise() {
+  function getTranspilerPromise () {
     return new Promise(function (resolve, reject) {
       transpiler.on('success', function (result) {
+        var middleware = result.__esModule ? result.default : result;
         transpilerPromise = null;
-        middlewarePromise = Promise.resolve(util.getDefaultExport(result));
-        resolve(middlewarePromise);
+        middlewarePromise = Promise.resolve(middleware);
+        resolve(middleware);
       });
       transpiler.on('error', reject);
     });
   }
 
-  function getMiddlewarePromise() {
+  function getMiddlewarePromise () {
     return middlewarePromise || transpilerPromise || getTranspilerPromise();
   }
 
   transpilerPromise = getTranspilerPromise().catch(util.logError);
 
   return function (req, res, next) {
-    getMiddlewarePromise().then(function (middleware) {
+    getMiddlewarePromise()
+    .then(function (middleware) {
       middleware(req, res, next);
     })
-    .catch(util.logError);
+    .catch(function (error) {
+      util.logError(error);
+      res.writeHead(500);
+      res.write('<h1>Server Error</h1>');
+      res.end();
+    });
   };
 };
