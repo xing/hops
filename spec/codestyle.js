f681261ade447ae3232aae33290568ef0f79c582
Daniel Dembach <daniel@dmbch.net>
2017-01-29 14:03:57 +0100

    tweak tests, esp. code style

--- spec/codestyle.js
+++ spec/codestyle.js
@@ -1,5 +1,19 @@
 /* eslint-env node, mocha */
 
+var semistandard = require('mocha-standard/semistandard');
+
 describe('code style', function () {
-  it('conforms to standard', require('mocha-standard/semistandard'));
+  this.timeout(5000);
+
+  it('conforms to semistandard', semistandard.files([
+    'bin/**/*.js',
+    'demo/src/**/*.js',
+    'etc/**/*.js',
+    'lib/**/*.js',
+    'middleware/**/*.js',
+    'plugin/**/*.js',
+    'renderer/**/*.js',
+    'spec/**/*.js',
+    'transpiler/**/*.js'
+  ]));
 });
