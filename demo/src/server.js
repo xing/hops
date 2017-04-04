33c5f88ffd7f509ca7a15687afaff0d76e5ea749
Daniel Dembach <daniel@dmbch.net>
2017-04-04 22:11:23 +0200

    update dependencies, especially for demo

--- demo/src/server.js
+++ demo/src/server.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import { ServerRouter, createServerRenderContext } from 'react-router';
+import { StaticRouter } from 'react-router';
 
 import App from './app';
 
@@ -9,31 +9,16 @@ import template from './template.tpl';
 import { version } from '../package.json';
 
 export default (req, res) => {
-  const context = createServerRenderContext();
-
-  let markup = renderToString(
-    <ServerRouter location={req.url} context={context}>
+  const context = {};
+  const markup = renderToString(
+    <StaticRouter location={req.url} context={context}>
       <App />
-    </ServerRouter>
+    </StaticRouter>
   );
-
-  const result = context.getResult();
-
-  if (result.redirect) {
-    res.writeHead(301, { Location: result.redirect.pathname });
-    res.end();
+  if (context.url) {
+    res.writeHead(301, { Location: context.url });
   } else {
-    if (result.missed) {
-      res.writeHead(404);
-
-      markup = renderToString(
-        <ServerRouter location={req.url} context={context}>
-          <App />
-        </ServerRouter>
-      );
-    }
-
     res.write(template({ markup, version }));
-    res.end();
   }
+  res.end();
 };
