0db15a3831be55d8bf15687fd26cfe17a88e006f
Daniel Dembach <daniel@dmbch.net>
2017-05-30 00:39:16 +0200

    refactor/improve hops-root

--- packages/root/index.js
+++ packages/root/index.js
@@ -1,14 +1,12 @@
 var path = require('path');
 var root = require('pkg-dir').sync();
 
-exports.toString = function () {
-  return root;
-};
-
-exports.resolve = function () {
-  return path.resolve.apply(path, [root].concat(Array.from(arguments)));
-};
+exports.resolve = path.resolve.bind(path, root);
 
 exports.require = function () {
-  return require(exports.resolve.apply(exports, arguments));
+  return require(exports.resolve.apply(null, arguments));
+};
+
+exports.toString = function () {
+  return root;
 };
