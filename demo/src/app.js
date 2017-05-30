c33c0d02d58e9b042669a2bc686011c516819e54
Daniel Dembach <daniel@dmbch.net>
2017-05-30 17:53:21 +0200

    update demo application

--- demo/src/app.js
+++ demo/src/app.js
@@ -2,16 +2,22 @@ import React from 'react';
 
 import { headline } from './styles.css';
 
-import { Route, Switch } from 'hops-react';
-import { render } from 'hops-redux-enhanced';
+import { Route, Switch, Miss } from 'hops-react';
+import { render } from 'hops-redux';
 
 const Home = () => (
   <h1 className={headline}>Hello World!</h1>
 );
 
+const Foo = () => (
+  <h1 className={headline}>Hello Foo!</h1>
+);
+
 const App = () => (
   <Switch>
     <Route exact path='/' component={Home} />
+    <Route path='/foo' component={Foo} />
+    <Miss />
   </Switch>
 );
 
