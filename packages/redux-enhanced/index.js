ee9ca20dcf347b2ae60e9d03475fa72fda672fdc
Daniel Dembach <daniel@dmbch.net>
2017-05-30 13:50:31 +0200

    add redux-enhanced package facilitating per-location actions

---
+++ packages/redux-enhanced/index.js
@@ -0,0 +1,68 @@
+var React = require('react');
+var ReactRouter = require('react-router-dom');
+var withSideEffect = require('react-side-effect');
+var createLocation = require('history/LocationUtils').createLocation;
+
+var HopsRedux = require('hops-redux');
+
+var Context = HopsRedux.Context;
+
+var Provider = ReactRouter.withRouter(
+  withSideEffect(
+    function reducePropsToState (propsList) {
+      return propsList[propsList.length - 1];
+    },
+    function handleStateChangeOnClient (props) {
+      if (props) {
+        props.setLocation(props.location);
+        props.dispatchAll();
+      }
+    },
+    function mapStateOnServer (props) {
+      return undefined;
+    }
+  )(function (props) {
+    return props.children ? React.Children.only(props.children) : null;
+  })
+);
+
+exports.Context = exports.createContext = Context.extend({
+  initialize: function (options) {
+    Context.prototype.initialize.call(this, options);
+    this.actionCreators = options.actionCreators || [];
+  },
+  bootstrap: function () {
+    return this.request ? this.dispatchAll() : Promise.resolve();
+  },
+  dispatchAll: function () {
+    var store = this.getStore();
+    var location = this.getLocation();
+    var request = this.request;
+    return Promise.all(this.actionCreators.map(function (createAction) {
+      return store.dispatch(createAction(location, request));
+    }));
+  },
+  setLocation: function (location) {
+    this.location = location;
+  },
+  getLocation: function () {
+    return this.location || createLocation(this.request.path);
+  },
+  enhanceElement: function (reactElement) {
+    return Context.prototype.enhanceElement.call(this, React.createElement(
+      Provider,
+      {
+        dispatchAll: this.dispatchAll.bind(this),
+        setLocation: this.setLocation.bind(this)
+      },
+      reactElement
+    ));
+  }
+});
+
+exports.render = function (reactElement, context) {
+  if (!(context instanceof exports.Context)) {
+    context = new exports.Context(context);
+  }
+  return HopsRedux.render(reactElement, context);
+};
