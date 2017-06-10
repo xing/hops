ba7ea98f47a1fb3cddbff3f5bc117a9a62319d74
Daniel Dembach <daniel@dmbch.net>
2017-06-10 23:22:29 +0200

    simplify demo

--- demo/src/app.js
+++ demo/src/app.js
@@ -3,36 +3,16 @@ import React from 'react';
 import { headline } from './styles.css';
 
 import { render, Route, Switch, Miss } from 'hops-react';
-import { createContext } from 'hops-redux';
 
 const Home = () => (
   <h1 className={headline}>Hello World!</h1>
 );
 
-const Foo = () => (
-  <h1 className={headline}>Hello Foo!</h1>
-);
-
 const App = () => (
   <Switch>
     <Route exact path='/' component={Home} />
-    <Route path='/foo' component={Foo} />
     <Miss />
   </Switch>
 );
 
-export default render(<App />, createContext({
-  reducers: {
-    foo: function (state, action) {
-      // console.log(state, action);
-      return Object.assign({}, state);
-    }
-  },
-  actionCreators: {
-    '/:foo': function (params) {
-      return function (dispatch) {
-        // console.log(params);
-      };
-    }
-  }
-}));
+export default render(<App />);
