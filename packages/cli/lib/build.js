86add46b79b988926a80532a441f2b450f467769
Daniel Dembach <daniel@dmbch.net>
2017-06-11 22:21:02 +0200

    clean up serve/build scripts

--- packages/cli/lib/build.js
+++ packages/cli/lib/build.js
@@ -3,16 +3,20 @@
 var webpack = require('webpack');
 
 var hopsConfig = require('hops-config');
-var webpackConfig = require(hopsConfig.buildConfig);
 
-var util = require('./util');
+var buildConfig = require(hopsConfig.buildConfig);
+var renderConfig = require(hopsConfig.renderConfig);
+var webpackConfig = [buildConfig, renderConfig];
 
-module.exports = function runBuild () {
-  webpack(webpackConfig).run(function (error, stats) {
-    if (error) {
-      util.logError(error.stack.toString());
-    } else {
-      util.logInfo(stats.toString({ chunks: false }));
-    }
-  });
+function defaultCallback (error, stats) {
+  var util = require('./util');
+  if (error) {
+    util.logError(error.stack.toString());
+  } else {
+    util.logInfo(stats.toString({ chunks: false }));
+  }
+}
+
+module.exports = function runBuild (callback) {
+  webpack(webpackConfig).run(callback || defaultCallback);
 };
