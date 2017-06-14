c3c25f9d064081c8c03b77bb32484bcefca18b3b
Daniel Dembach <daniel@dmbch.net>
2017-06-14 23:49:46 +0200

    clean up build dir before each run

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -2,6 +2,10 @@
 'use strict';
 
 var program = require('commander');
+var rimraf = require('rimraf');
+
+var hopsConfig = require('hops-config');
+var hopsRoot = require('hops-root');
 
 program
 .version(require('./package.json').version)
@@ -9,18 +13,20 @@ program
 .option('-p, --port', 'server TCP port')
 .arguments('<command>')
 .action(function run (command) {
-  switch (command) {
-    case 'start':
-      return run(process.env.NODE_ENV === 'production' ? 'serve' : 'develop');
-    case 'serve':
-      return require('./lib/serve')(program.port);
-    case 'develop':
-      return require('./lib/develop')(program.port);
-    case 'build':
-      return require('./lib/build')();
-    default:
-      require('./lib/util').logError('invalid command: ' + command);
-      process.exit(1);
-  }
+  rimraf(hopsRoot.resolve(hopsConfig.buildDir), function () {
+    switch (command) {
+      case 'start':
+        return run(process.env.NODE_ENV === 'production' ? 'serve' : 'develop');
+      case 'serve':
+        return require('./lib/serve')(program.port);
+      case 'develop':
+        return require('./lib/develop')(program.port);
+      case 'build':
+        return require('./lib/build')();
+      default:
+        require('./lib/util').logError('invalid command: ' + command);
+        process.exit(1);
+    }
+  });
 })
 .parse(process.argv);
