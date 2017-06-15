c7d56c4833b22a82f5654cb0ca508383e972dbb8
Daniel Dembach <daniel@dmbch.net>
2017-06-16 00:13:45 +0200

    flesh out npm-based configuration system

--- packages/config/index.js
+++ packages/config/index.js
@@ -1,37 +1,48 @@
 'use strict';
 
-var fs = require('fs');
 var path = require('path');
+var url = require('url');
 
 var hopsRoot = require('hops-root');
 
+// https://docs.npmjs.com/misc/scripts#special-packagejson-config-object
 var config = module.exports = {
-  buildConfig: require.resolve('./configs/build'),
-  developConfig: require.resolve('./configs/develop'),
-  renderConfig: require.resolve('./configs/render'),
+  https: process.env.npm_package_config_https || false,
+  host: process.env.npm_package_config_host || '0.0.0.0',
+  port: process.env.npm_package_config_port || 8080,
+  buildDir: process.env.npm_package_config_builddir || 'build',
+  buildConfig: process.env.npm_package_config_build ||
+    require.resolve('./configs/build'),
+  developConfig: process.env.npm_package_config_develop ||
+    require.resolve('./configs/develop'),
+  renderConfig: process.env.npm_package_config_render ||
+    require.resolve('./configs/render'),
   browsers: '> 5%, last 1 version',
-  buildDir: 'build',
   resolve: {},
   loaders: {},
   plugins: {},
+  devServer: {},
   locations: []
 };
 
-if (fs.existsSync(hopsRoot.resolve('package.json'))) {
-  var pkgConfig = hopsRoot.require('package.json').hops || {};
-  if (pkgConfig.extends) {
-    try {
-      require.resolve(pkgConfig.extends);
-      Object.assign(config, require(pkgConfig.extends));
-    } catch (e) {
-      Object.assign(config, hopsRoot.require(pkgConfig.extends));
-    }
+if (process.env.npm_package_config_extends) {
+  var baseFile = process.env.npm_package_config_extends;
+  try {
+    require.resolve(baseFile);
+    Object.assign(config, require(baseFile));
+  } catch (e) {
+    Object.assign(config, hopsRoot.require(baseFile));
   }
-  Object.assign(config, pkgConfig);
 }
 
 ['buildConfig', 'developConfig', 'renderConfig'].forEach(function (key) {
   if (!path.isAbsolute(config[key])) {
     config[key] = hopsRoot.resolve(config[key]);
   }
+});
+
+config.address = url.format({
+  protocol: config.https ? 'https' : 'http',
+  hostname: config.host,
+  port: config.port
 });
--- packages/config/lib/server-config.js
+++ packages/config/lib/server-config.js
@@ -1,8 +1,26 @@
-var createMiddleware = require('hops-middleware');
-var hopsConfig = require('..');
+'use strict';
 
-module.exports = function setup (watchOptions) {
-  return function (app) {
+var hopsRoot = require('hops-root');
+var createMiddleware = require('hops-middleware');
+
+var hopsConfig = require('..');
+var devServerConfig = hopsConfig.devServer;
+
+var watchOptions = {
+  aggregateTimeout: 300,
+  ignored: /node_modules/
+};
+
+var defaultDevServerConfig = {
+  contentBase: hopsRoot.resolve(hopsConfig.buildDir),
+  hot: true,
+  overlay: {
+    warnings: true,
+    errors: true
+  },
+  stats: 'errors-only',
+  watchOptions: watchOptions,
+  setup: function (app) {
     var webpackConfig = require(hopsConfig.renderConfig);
     var middleware = createMiddleware(webpackConfig, watchOptions);
     if (hopsConfig.locations.length) {
@@ -22,5 +40,12 @@ module.exports = function setup (watchOptions) {
     } else {
       app.all('*', middleware);
     }
-  };
+  }
+};
+
+module.exports = function () {
+  if (typeof devServerConfig === 'function') {
+    return devServerConfig(defaultDevServerConfig);
+  }
+  return Object.assign({}, defaultDevServerConfig, devServerConfig);
 };
