82f6d141823b950f1477e68918df8040d06a2ef7
Daniel Dembach <daniel@dmbch.net>
2017-06-25 23:10:14 +0200

    implement internal cssnext replacement

---
+++ packages/config/lib/postcss-necsst.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var postcss = require('postcss');
+var autoprefixer = require('autoprefixer');
+var isSupported = require('caniuse-api').isSupported;
+
+var features = [
+  ['css-variables', 'postcss-custom-properties'],
+  ['css-image-set', 'postcss-image-set-polyfill'],
+  'postcss-nesting',
+  'postcss-custom-media',
+  'postcss-media-minmax',
+  'postcss-custom-selectors',
+  ['css-case-insensitive', 'postcss-attribute-case-insensitive'],
+  ['css-rebeccapurple', 'postcss-color-rebeccapurple'],
+  'postcss-color-hwb',
+  'postcss-color-hsl',
+  'postcss-color-rgb',
+  'postcss-color-gray',
+  ['css-rrggbbaa', 'postcss-color-hex-alpha'],
+  'postcss-color-function',
+  'postcss-font-family-system-ui',
+  'postcss-font-variant',
+  ['css-all', 'css-initial-value', 'postcss-initial'],
+  ['css-matches-pseudo', 'postcss-selector-matches'],
+  ['css-not-sel-list', 'postcss-selector-not'],
+  'postcss-pseudo-class-any-link',
+  ['wordwrap', 'postcss-replace-overflow-wrap']
+];
+
+module.exports = postcss.plugin('postcss-necsst', function (options) {
+  var processor = postcss();
+  features.forEach(function (feature) {
+    if (Array.isArray(feature)) {
+      var conditions = feature;
+      feature = conditions.pop();
+      if (conditions.find(function (condition) {
+        return !isSupported(condition, options.browsers);
+      })) {
+        processor.use(require(feature)());
+      }
+    } else {
+      processor.use(require(feature)());
+    }
+  });
+  processor.use(
+    autoprefixer(options.browsers ? { browsers: options.browsers } : {})
+  );
+  return processor;
+});
