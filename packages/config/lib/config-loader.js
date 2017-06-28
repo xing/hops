40b0764ef243b568f363e8513bf790c67f74c204
Daniel Dembach <daniel@dmbch.net>
2017-06-28 17:09:54 +0200

    simplify config loader

--- packages/config/lib/config-loader.js
+++ packages/config/lib/config-loader.js
@@ -3,12 +3,8 @@
 var hopsConfig = require('..');
 
 module.exports = function () {
-  var config = Object.keys(hopsConfig).reduce(function (result, key) {
-    if (!/(?:config|script|file|dir)s?$/i.test(key)) {
-      result[key] = hopsConfig[key];
-    }
-    return result;
-  }, {});
   this.cacheable();
-  return 'module.exports = ' + JSON.stringify(config);
+  return 'module.exports = ' + JSON.stringify(hopsConfig).replace(
+    new RegExp(hopsConfig.appDir, 'g'), ''
+  );
 };
