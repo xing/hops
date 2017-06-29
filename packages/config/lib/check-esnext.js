35d4328ab3b8c9b8ddaf642691fc69f2cc9e44a2
Daniel Dembach <daniel@dmbch.net>
2017-06-29 23:00:55 +0200

    simplify check-esnext
    custom module path handling is not required here, webpack already does
    it for us.

--- packages/config/lib/check-esnext.js
+++ packages/config/lib/check-esnext.js
@@ -4,41 +4,35 @@ var fs = require('fs');
 var path = require('path');
 
 var getPkgDir = require('pkg-dir').sync;
-var resolve = require('resolve').sync;
 
 var appDir = getPkgDir();
-var appModuleDir = path.resolve(appDir, 'node_modules');
-
-var cache = {};
+var moduleDir = path.resolve(appDir, 'node_modules');
+var shimDir = path.resolve(__dirname, '..', 'shims');
 
 function checkEsnext (filepath) {
-  if (
-    /\.mjs$/.test(filepath) ||
-    filepath.indexOf(path.resolve(__dirname, '..', 'shims')) === 0
-  ) {
+  if (/\.mjs$/.test(filepath) || filepath.indexOf(shimDir) === 0) {
     return true;
   }
   var pkgDir = getPkgDir(path.dirname(filepath));
   var packageJsonPath = path.resolve(pkgDir, 'package.json');
   if (fs.existsSync(packageJsonPath)) {
-    var json = fs.readFileSync(packageJsonPath);
+    var json = fs.readFileSync(packageJsonPath, 'utf8');
     return /"(module|((e|j)snext(:(browser|server|main))?))":/m.test(json);
   } else {
     return false;
   }
 }
 
+var cache = {};
 module.exports = function checkEsnextCached (module) {
   if (!(module in cache)) {
     if (path.isAbsolute(module)) {
       cache[module] = (
         (module.indexOf(appDir) === 0) &&
-        ((module.indexOf(appModuleDir) === -1) || checkEsnext(module))
+        ((module.indexOf(moduleDir) === -1) || checkEsnext(module))
       );
     } else {
-      cache[module] = checkEsnextCached(resolve(module, {
-        moduleDirectory: ['node_modules', path.resolve(appDir, 'packages')]
-      }));
+      cache[module] = checkEsnextCached(require.resolve(module));
     }
   }
   return cache[module];
