b64dcba51edd5aadeb67a50837763531e4c5967b
Daniel Dembach <daniel@dmbch.net>
2017-07-05 23:44:54 +0200

    make demo a bit more interesting

--- demo/src/app.js
+++ demo/src/app.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { render, Miss } from 'hops-react';
+import { createContext } from 'hops-redux';
+import { connect } from 'react-redux';
 
 import { headline } from './styles.css';
 
-const Home = () => (
-  <h1 className={headline}>Hello World!</h1>
-);
+const withMessage = connect(state => state.foo);
+
+const Home = withMessage(props => (
+  <h1 className={headline}>{ props.message }</h1>
+));
 
 const App = () => (
   <Switch>
     <Route exact path='/' component={Home} />
-    <Route path='/foo' component={Home} />
     <Miss />
   </Switch>
 );
 
-export default render(<App />);
+export default render(<App />, createContext({
+  reducers: {
+    'foo': state => state || { message: 'Hello World!' }
+  }
+}));
