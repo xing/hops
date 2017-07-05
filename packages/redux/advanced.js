fe510960ee3acb0e2751ed1df54e8f62982009da
Robin Drexler <robin.drexler@xing.com>
2017-07-05 17:04:27 +0200

    chore(hops-redux): prettify file

--- packages/redux/advanced.js
+++ packages/redux/advanced.js
@@ -9,59 +9,67 @@ var Context = require('./basic').Context;
 
 var Dispatcher = ReactRouter.withRouter(
   withSideEffect(
-    function reducePropsToState (propsList) {
+    function reducePropsToState(propsList) {
       return propsList[propsList.length - 1];
     },
-    function handleStateChangeOnClient (props) {
+    function handleStateChangeOnClient(props) {
       if (props) {
         props.dispatchAll(props.location);
       }
     },
-    function mapStateOnServer (props) {
+    function mapStateOnServer(props) {
       return undefined;
     }
-  )(function (props) {
+  )(function(props) {
     return props.children ? React.Children.only(props.children) : null;
   })
 );
 
 exports.Context = exports.createContext = Context.extend({
-  initialize: function (options) {
+  initialize: function(options) {
     Context.prototype.initialize.call(this, options);
     this.actionCreators = Object.assign({}, options.actionCreators);
   },
-  bootstrap: function () {
+  bootstrap: function() {
     if (this.request) {
       return this.dispatchAll(createLocation(this.request.path));
     }
     return Promise.resolve();
   },
-  dispatchAll: function (location) {
+  dispatchAll: function(location) {
     var store = this.getStore();
     var keys = Object.keys(this.actionCreators);
     var actionCreators = this.actionCreators;
-    return Promise.all(keys.map(function dispatchOne (key) {
-      var match = ReactRouter.matchPath(
-        location.pathname,
-        { path: key, exact: true, strict: true }
-      );
-      if (!match) {
-        return Promise.resolve();
-      }
-      if (Array.isArray(actionCreators[key])) {
-        return Promise.all(actionCreators[key].map(function (createAction) {
-          return store.dispatch(createAction(match.params));
-        }));
-      } else {
-        return store.dispatch(actionCreators[key](match.params));
-      }
-    }));
+    return Promise.all(
+      keys.map(function dispatchOne(key) {
+        var match = ReactRouter.matchPath(location.pathname, {
+          path: key,
+          exact: true,
+          strict: true
+        });
+        if (!match) {
+          return Promise.resolve();
+        }
+        if (Array.isArray(actionCreators[key])) {
+          return Promise.all(
+            actionCreators[key].map(function(createAction) {
+              return store.dispatch(createAction(match.params));
+            })
+          );
+        } else {
+          return store.dispatch(actionCreators[key](match.params));
+        }
+      })
+    );
   },
-  enhanceElement: function (reactElement) {
-    return Context.prototype.enhanceElement.call(this, React.createElement(
-      Dispatcher,
-      { dispatchAll: this.dispatchAll.bind(this) },
-      reactElement
-    ));
+  enhanceElement: function(reactElement) {
+    return Context.prototype.enhanceElement.call(
+      this,
+      React.createElement(
+        Dispatcher,
+        { dispatchAll: this.dispatchAll.bind(this) },
+        reactElement
+      )
+    );
   }
 });
