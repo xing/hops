a868a0a5d4b0289e25569443af4fdf2f9b1fb5f8
Daniel Dembach <daniel@dmbch.net>
2017-07-06 14:15:36 +0200

    make demo even more interesting

--- demo/src/app.js
+++ demo/src/app.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import { render, Miss } from 'hops-react';
 import { createContext } from 'hops-redux';
-import { connect } from 'react-redux';
 
 import { headline } from './styles.css';
 
 const withMessage = connect(state => state.foo);
 
 const Home = withMessage(props => (
-  <h1 className={headline}>{ props.message }</h1>
+  <div>
+    <Helmet>
+      <title>Hops Demo</title>
+    </Helmet>
+    <h1 className={headline}>{ props.message }</h1>
+  </div>
 ));
 
 const App = () => (
