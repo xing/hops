029d43a13a0062a95cc737f1e12d4cd2f3ecb0db
Daniel Dembach <daniel@dmbch.net>
2017-07-16 21:56:42 +0200

    use webpack-merge to augment webpack configs

--- packages/cli/commands/build.js
+++ packages/cli/commands/build.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var webpack = require('webpack');
+var merge = require('webpack-merge');
 
 var hopsConfig = require('hops-config');
 var HopsPlugin = require('hops-plugin');
@@ -8,22 +9,34 @@ var HopsPlugin = require('hops-plugin');
 var buildConfig = require(hopsConfig.buildConfig);
 var nodeConfig = require(hopsConfig.nodeConfig);
 
+var mergeWithPlugins = merge.strategy({ plugins: 'append' });
+
 function getWebpackConfig (program) {
   if (program.static) {
-    buildConfig.plugins.push(
-      new HopsPlugin(
-        hopsConfig.locations.map(function (location) {
-          return hopsConfig.basePath + location;
-        }),
-        nodeConfig
-      ),
-      new webpack.ProgressPlugin()
+    return mergeWithPlugins(
+      buildConfig,
+      {
+        plugins: [
+          new HopsPlugin(
+            hopsConfig.locations.map(function (location) {
+              return hopsConfig.basePath + location;
+            }),
+            nodeConfig
+          ),
+          new webpack.ProgressPlugin()
+        ]
+      }
     );
-    return buildConfig;
   } else {
     return [buildConfig, nodeConfig].map(function (config) {
-      config.plugins.push(new webpack.ProgressPlugin());
-      return config;
+      return mergeWithPlugins(
+        config,
+        {
+          plugins: [
+            new webpack.ProgressPlugin()
+          ]
+        }
+      );
     });
   }
 }
