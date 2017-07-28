b3f4a63ee3f35aba4c95eab27934b17236686c24
Daniel Dembach <daniel@dmbch.net>
2017-07-29 00:32:46 +0200

    get rid of obsolete comment

--- packages/jest-preset/transform.js
+++ packages/jest-preset/transform.js
@@ -2,21 +2,6 @@ var babelJest = require('babel-jest');
 
 var hopsBabelConfig = require('hops-config/loaders/babel').node.use.options;
 
-// presets look like this
-// we try to figure out at which position "env" is and and set modules to "commonjs" to transpile import statements in jest
-
-// presets: [
-//   [
-//     'env',
-//     {
-//       modules: false,
-//       useBuiltIns: true,
-//       targets: targets
-//     }
-//   ],
-//   'react'
-// ]
-
 var envConfig = hopsBabelConfig.presets.find(function (preset) {
   return Array.isArray(preset) && preset[0] === 'env' && preset[1];
 });
