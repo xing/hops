2eabaf9c44bf9f5fe0019ba0ea032b78afb7383f
Robin Drexler <robin.drexler@xing.com>
2017-07-28 14:18:28 +0200

    feat(demo): add jest

--- demo/src/app.js
+++ demo/src/app.js
@@ -6,28 +6,19 @@ import { connect } from 'react-redux';
 import { render, Miss } from 'hops-react';
 import { createContext } from 'hops-redux';
 
-import { headline } from './styles.css';
+import Home from './home';
 
-const withMessage = connect(state => state.foo);
-
-const Home = withMessage(props => (
-  <div>
-    <Helmet>
-      <title>Hops Demo</title>
-    </Helmet>
-    <h1 className={headline}>{ props.message }</h1>
-  </div>
-));
-
-const App = () => (
+const App = () =>
   <Switch>
-    <Route exact path='/' component={Home} />
+    <Route exact path="/" component={Home} />
     <Miss />
-  </Switch>
-);
+  </Switch>;
 
-export default render(<App />, createContext({
-  reducers: {
-    'foo': state => state || { message: 'Hello World!' }
-  }
-}));
+export default render(
+  <App />,
+  createContext({
+    reducers: {
+      foo: state => state || { message: 'Hello World!' }
+    }
+  })
+);
