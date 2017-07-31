a26b78a031d7ad7db8c08645d9de51b02fdd7b4f
Daniel Dembach <daniel@dmbch.net>
2017-07-31 23:22:53 +0200

    simplify jest/babel setup

--- packages/jest-preset/transform.js
+++ packages/jest-preset/transform.js
@@ -2,13 +2,7 @@ var babelJest = require('babel-jest');
 
 var hopsBabelConfig = require('hops-config/loaders/babel').node.use.options;
 
-var envConfig = hopsBabelConfig.presets.find(function (preset) {
-  return Array.isArray(preset) && preset[0] === 'env' && preset[1];
-});
-
-if (envConfig) {
-  envConfig[1].modules = 'commonjs';
-}
+hopsBabelConfig.presets[0][1].modules = 'commonjs';
 
 module.exports = babelJest.createTransformer({
   presets: hopsBabelConfig.presets,
