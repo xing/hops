d52acbe619be1bca5ac463e0ef23d6ef59b19c81
ZauberNerd <zaubernerd@zaubernerd.de>
2017-08-16 17:21:43 +0200

    chore: semistandard uses parser "babel-eslint" to recognize flowtype

--- spec/codestyle.js
+++ spec/codestyle.js
@@ -3,11 +3,16 @@
 var semistandard = require('mocha-standard/semistandard');
 
 describe('code style', function () {
-  this.timeout(5000);
+  this.timeout(10000);
 
-  it('conforms to semistandard', semistandard.files([
-    'packages/**/*.js',
-    'demo/src/*.js',
-    'spec/**/*.js'
-  ]));
+  it('conforms to semistandard', semistandard.files(
+    [
+      'packages/**/*.js',
+      'demo/src/*.js',
+      'spec/**/*.js'
+    ], {
+      parser: 'babel-eslint',
+      plugins: ['flowtype']
+    }
+  ));
 });
