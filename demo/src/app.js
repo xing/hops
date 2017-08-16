50fd884100a1677290405662b672e8900fb59494
ZauberNerd <zaubernerd@zaubernerd.de>
2017-08-16 16:42:26 +0200

    refactor(demo): move "Home" into feature folder & enable flowtype

--- demo/src/app.js
+++ demo/src/app.js
@@ -1,10 +1,11 @@
+// @flow
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import { render, Miss } from 'hops-react';
 import { createContext } from 'hops-redux';
 
-import Home from './home';
+import { Home } from './home';
 
 const App = () => (
   <Switch>
