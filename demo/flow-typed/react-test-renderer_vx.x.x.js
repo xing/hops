b7c47b73d40bade57d0ad0e3aa2955550c25c4b6
ZauberNerd <zaubernerd@zaubernerd.de>
2017-09-12 12:04:57 +0200

    chore(demo): adds flowtype definitions for react-test-renderer
    The flow-typed repository doesn't have type definitions for
    "react-test-renderer" yet. This commit adds a rudimentary definition
    of type definitions for "react-test-renderer" to the "demo" package.

---
+++ demo/flow-typed/react-test-renderer_vx.x.x.js
@@ -0,0 +1,19 @@
+declare module 'react-test-renderer' {
+
+  declare type TestRendererOptions = {
+    createNodeMock: (element: ReactElement<any>) => any;
+  };
+
+  declare module.exports: {
+    create(
+      Component: React$Element<*>,
+      options?: TestRendererOptions
+    ): {
+      toJSON(): {
+        props: $PropertyType<Component, 'props'>,
+        type: $PropertyType<Component, 'type'>,
+        children: $PropertyType<Component, 'children'>,
+      }
+    }
+  };
+}
