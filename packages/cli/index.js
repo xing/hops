cb729c08a78792fcc1e3dc302ce3e892c0c41578
ZauberNerd <zaubernerd@zaubernerd.de>
2017-09-29 18:45:34 +0200

    refactor(cli): move express server into hops-express

--- packages/cli/index.js
+++ packages/cli/index.js
@@ -1,16 +1,29 @@
 'use strict';
 
+var hopsBuild = require('hops-build');
+var hopsExpress = require('hops-express');
+
+function runServe (program, callback) {
+  hopsBuild.runBuild(program, function (error) {
+    if (error) {
+      callback ? callback(error) : console.error(error.stack.toString());
+    } else {
+      hopsExpress.startServer(callback);
+    }
+  });
+}
+
 module.exports = {
   runBuild: function callRunBuild () {
-    return require('hops-build').runBuild.apply(null, arguments);
+    return hopsBuild.runBuild.apply(null, arguments);
   },
   runDevelop: function callRunDevelop () {
-    return require('hops-build').runServer.apply(null, arguments);
+    return hopsBuild.runServer.apply(null, arguments);
   },
   runServe: function callRunServe () {
-    return require('./commands/serve').apply(null, arguments);
+    return runServe.apply(null, arguments);
   },
   startServer: function callStartServer () {
-    return require('./lib/server').apply(null, arguments);
+    return hopsExpress.startServer.apply(null, arguments);
   }
 };
