db529d0821ca522bc849fa97b6faefe8c8248234
ZauberNerd <zaubernerd@zaubernerd.de>
2017-10-12 17:54:25 +0200

    feat(template-minimal): add a minimal example template without react

---
+++ packages/template-minimal/src/browser.js
@@ -0,0 +1,11 @@
+import styles from './styles.css';
+
+export default () => {
+  const root = document.getElementById('app');
+
+  const headline = document.createElement('h1');
+  headline.innerText = 'Hello Hops!';
+  headline.className = styles.headline;
+
+  root.appendChild(headline);
+};
