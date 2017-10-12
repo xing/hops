3b2e39c1262d66ec553035804e641cffd30ae002
Daniel Dembach <daniel@dmbch.net>
2017-10-12 17:51:54 +0200

    refactor(build-config): simplify freezing...

--- packages/build-config/index.js
+++ packages/build-config/index.js
@@ -2,21 +2,7 @@
 
 var hopsConfig = require('hops-config');
 
-function freeze (config) {
-  return Object.freeze(
-    Object.keys(config).reduce(function (result, key) {
-      var descriptor = { enumerable: true };
-      if (typeof config[key] === 'function') {
-        descriptor.get = config[key];
-      } else {
-        descriptor.value = config[key];
-      }
-      return Object.defineProperty(result, key, descriptor);
-    }, {})
-  );
-}
-
-module.exports = freeze({
+module.exports = Object.freeze({
   buildConfig: hopsConfig.buildConfig
     ? hopsConfig.buildConfig
     : require.resolve('./configs/build'),
