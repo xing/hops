e827007f90f5c9a3001dcf672b5ba477440ceaad
ZauberNerd <zaubernerd@zaubernerd.de>
2017-10-13 14:23:58 +0200

    feat(local-cli): add --production/-p flag to build, serve and start

--- packages/local-cli/commands/build.js
+++ packages/local-cli/commands/build.js
@@ -16,11 +16,22 @@ module.exports = function buildCommand (callback) {
       clean: {
         alias: 'c',
         default: true,
-        describe: 'Clean up artifacts in build / cache directories before building',
+        describe: 'Clean up artifacts in build / cache directories before ' +
+          'building',
+        type: 'boolean'
+      },
+      production: {
+        alias: 'p',
+        default: false,
+        describe: 'Minifies the output, generates source maps and removes ' +
+          'React developer warnings',
         type: 'boolean'
       }
     },
     handler: function buildHandler (argv) {
+      if (argv.production) {
+        process.env.NODE_ENV = 'production';
+      }
       hopsBuild.runBuild(argv, callback);
     }
   };
--- packages/local-cli/commands/start.js
+++ packages/local-cli/commands/start.js
@@ -11,11 +11,22 @@ module.exports = function startCommand (callback) {
       clean: {
         alias: 'c',
         default: true,
-        describe: 'Clean up artifacts in build / cache directories before building',
+        describe: 'Clean up artifacts in build / cache directories before ' +
+          'building',
+        type: 'boolean'
+      },
+      production: {
+        alias: 'p',
+        default: false,
+        describe: 'Minifies the output, generates source maps and removes ' +
+          'React developer warnings',
         type: 'boolean'
       }
     },
     handler: function startHandler (argv) {
+      if (argv.production) {
+        process.env.NODE_ENV = 'production';
+      }
       if (process.env.NODE_ENV === 'production') {
         hopsExpress.startServer(callback);
       } else {
