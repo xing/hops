559ec99aed2cd63fcc94d752aee2f7e62414b71e
ZauberNerd <zaubernerd@zaubernerd.de>
2017-10-13 14:23:58 +0200

    feat(local-cli): add --production/-p flag to build, serve and start

--- packages/local-cli/commands/serve.js
+++ packages/local-cli/commands/serve.js
@@ -7,8 +7,19 @@ module.exports = function serveCommand (callback) {
     command: 'serve',
     describe: 'Starts a production-ready Node.js server to serve your ' +
     'application',
-    builder: {},
-    handler: function serveHandler () {
+    builder: {
+      production: {
+        alias: 'p',
+        default: false,
+        describe: 'Minifies the output, generates source maps and removes ' +
+          'React developer warnings',
+        type: 'boolean'
+      }
+    },
+    handler: function serveHandler (argv) {
+      if (argv.production) {
+        process.env.NODE_ENV = 'production';
+      }
       hopsExpress.startServer(callback);
     }
   };
