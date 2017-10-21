c76c91f7cf1b6f5201712b2dbd30016f64fe39d3
Daniel Dembach <daniel@dmbch.net>
2017-10-22 00:26:43 +0200

    feat(local-cli): make local-cli extensible
    local-cli now finds commands prefixed with hops-* in node_modules/.bin, registers them
    
    closes #244

--- packages/local-cli/commands/start.js
+++ packages/local-cli/commands/start.js
@@ -1,44 +1,50 @@
+#!/usr/bin/env node
 'use strict';
 
-var hopsBuild = require('hops-build');
-var hopsExpress = require('hops-express');
+var pm = require('../lib/package-manager');
 
-module.exports = function startCommand (callback) {
-  return {
-    command: 'start',
-    describe: 'Starts a development or production server, based on NODE_ENV',
-    builder: {
-      static: {
-        alias: 's',
-        default: false,
-        describe: 'Sets mode to static',
-        type: 'boolean'
-      },
-      clean: {
-        alias: 'c',
-        default: true,
-        describe: 'Clean up artifacts in build / cache directories before ' +
-          'building',
-        type: 'boolean'
-      },
-      production: {
-        alias: 'p',
-        default: false,
-        describe: 'Minifies the output, generates source maps and removes ' +
-          'React developer warnings',
-        type: 'boolean'
-      }
+module.exports = {
+  command: 'start',
+  describe: 'Starts a development or production server, based on NODE_ENV',
+  builder: {
+    static: {
+      alias: 's',
+      default: false,
+      describe: 'Sets mode to static',
+      type: 'boolean'
     },
-    handler: function startHandler (argv) {
-      if (argv.production) {
-        process.env.NODE_ENV = 'production';
-      }
-      process.env.HOPS_MODE = argv.static ? 'static' : 'dynamic';
-      if (process.env.NODE_ENV === 'production') {
-        hopsExpress.startServer(callback);
-      } else {
-        hopsBuild.runServer(argv, callback);
-      }
+    clean: {
+      alias: 'c',
+      default: true,
+      describe: 'Clean up artifacts in build / cache directories before ' +
+        'building',
+      type: 'boolean'
+    },
+    production: {
+      alias: 'p',
+      default: false,
+      describe: 'Minifies the output, generates source maps and removes ' +
+        'React developer warnings',
+      type: 'boolean'
     }
-  };
+  },
+  handler: function startHandler (argv) {
+    if (argv.production) {
+      process.env.NODE_ENV = 'production';
+    }
+    process.env.HOPS_MODE = argv.static ? 'static' : 'dynamic';
+    var packageName = (process.env.NODE_ENV === 'production')
+      ? 'hops-express'
+      : 'hops-build';
+    if (pm.isPackageInstalled(packageName)) {
+      require(packageName).runServer(argv);
+    } else {
+      console.error('Package missing: ', packageName);
+      process.exit(1);
+    }
+  }
 };
+
+if (require.main === module) {
+  require('hops-local-cli').run(module.exports);
+}
