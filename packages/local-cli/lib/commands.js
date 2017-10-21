442c97830d9bd3dc1bd8e6019d7226f05e94e503
Daniel Dembach <daniel@dmbch.net>
2017-10-22 00:54:36 +0200

    test: adapt tests to extensible local cli

--- packages/local-cli/lib/commands.js
+++ packages/local-cli/lib/commands.js
@@ -7,10 +7,14 @@ var hopsConfig = require('hops-config');
 
 var binDir = path.resolve(hopsConfig.appDir, 'node_modules', '.bin');
 
-module.exports = fs.readdirSync(binDir)
-  .filter(function (command) {
-    return command.indexOf('hops-') === 0;
-  })
-  .map(function (command) {
-    return path.join(binDir, command);
-  });
+if (fs.existsSync(binDir)) {
+  module.exports = fs.readdirSync(binDir)
+    .filter(function (command) {
+      return command.indexOf('hops-') === 0;
+    })
+    .map(function (command) {
+      return path.join(binDir, command);
+    });
+} else {
+  module.exports = [];
+}
