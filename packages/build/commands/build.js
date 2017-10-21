f4dc776c7f21ba497f87f35e9d49fb7bb6aa1833
Daniel Dembach <daniel@dmbch.net>
2017-10-22 00:29:53 +0200

    feat(build): add standalone commands compatible with local-cli

---
+++ packages/build/commands/build.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+'use strict';
+
+var hopsBuild = require('..');
+
+module.exports = {
+  command: 'build',
+  describe: 'Builds the browser and server JS bundles',
+  builder: {
+    static: {
+      alias: 's',
+      default: false,
+      describe: 'Statically build locations',
+      type: 'boolean'
+    },
+    clean: {
+      alias: 'c',
+      default: true,
+      describe: 'Clean up artifacts in build / cache directories before ' +
+        'building',
+      type: 'boolean'
+    },
+    production: {
+      alias: 'p',
+      default: false,
+      describe: 'Minifies the output, generates source maps and removes ' +
+        'React developer warnings',
+      type: 'boolean'
+    }
+  },
+  handler: function buildHandler (argv) {
+    if (argv.production) {
+      process.env.NODE_ENV = 'production';
+    }
+    process.env.HOPS_MODE = argv.static ? 'static' : 'dynamic';
+    hopsBuild.runBuild(argv);
+  }
+};
+
+if (require.main === module) {
+  require('hops-local-cli').run(module.exports);
+}
---
+++ packages/build/commands/develop.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+'use strict';
+
+var hopsBuild = require('..');
+
+module.exports = {
+  command: 'develop',
+  describe: 'Starts a webpack-dev-server to enable local development with ' +
+    'hot code reloading',
+  builder: {
+    static: {
+      alias: 's',
+      default: false,
+      describe: 'Serve app in hot mode with static env variable turned on',
+      type: 'boolean'
+    },
+    clean: {
+      alias: 'c',
+      default: true,
+      describe: 'Clean up artifacts in build / cache directories before ' +
+        'building',
+      type: 'boolean'
+    }
+  },
+  handler: function developHandler (argv) {
+    process.env.HOPS_MODE = argv.static ? 'static' : 'dynamic';
+    hopsBuild.runServer(argv);
+  }
+};
+
+if (require.main === module) {
+  require('hops-local-cli').run(module.exports);
+}
