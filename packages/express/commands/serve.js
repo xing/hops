7492703a54de7b82e7073ac7cce00704b45a6578
Daniel Dembach <daniel@dmbch.net>
2017-10-23 17:59:04 +0200

    refactor(express): adopt functional CLI extension API

--- packages/express/commands/serve.js
+++ packages/express/commands/serve.js
@@ -3,34 +3,36 @@
 
 var hopsExpress = require('..');
 
-module.exports = {
-  command: 'serve',
-  describe: 'Starts a production-ready Node.js server to serve your ' +
-  'application',
-  builder: {
-    static: {
-      alias: 's',
-      default: false,
-      describe: 'Serve built app with static env variable turned on',
-      type: 'boolean'
+module.exports = function defineServeCommand (args) {
+  args.command({
+    command: 'serve',
+    describe: 'Starts a production-ready Node.js server to serve your ' +
+    'application',
+    builder: {
+      static: {
+        alias: 's',
+        default: false,
+        describe: 'Serve built app with static env variable turned on',
+        type: 'boolean'
+      },
+      production: {
+        alias: 'p',
+        default: false,
+        describe: 'Minifies the output, generates source maps and removes ' +
+          'React developer warnings',
+        type: 'boolean'
+      }
     },
-    production: {
-      alias: 'p',
-      default: false,
-      describe: 'Minifies the output, generates source maps and removes ' +
-        'React developer warnings',
-      type: 'boolean'
+    handler: function serveHandler (argv) {
+      if (argv.production) {
+        process.env.NODE_ENV = 'production';
+      }
+      process.env.HOPS_MODE = argv.static ? 'static' : 'dynamic';
+      hopsExpress.runServer();
     }
-  },
-  handler: function serveHandler (argv) {
-    if (argv.production) {
-      process.env.NODE_ENV = 'production';
-    }
-    process.env.HOPS_MODE = argv.static ? 'static' : 'dynamic';
-    hopsExpress.runServer();
-  }
+  });
 };
 
 if (require.main === module) {
-  require('hops-local-cli').run(module.exports);
+  require('hops-local-cli').run(module.exports, 'serve');
 }
