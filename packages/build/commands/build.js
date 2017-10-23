0651c289fe76a2bcc031d2e74d906812de6c92f4
Daniel Dembach <daniel@dmbch.net>
2017-10-23 17:58:24 +0200

    refactor(build): adopt functional CLI extension API

--- packages/build/commands/build.js
+++ packages/build/commands/build.js
@@ -3,40 +3,42 @@
 
 var hopsBuild = require('..');
 
-module.exports = {
-  command: 'build',
-  describe: 'Builds the browser and server JS bundles',
-  builder: {
-    static: {
-      alias: 's',
-      default: false,
-      describe: 'Statically build locations',
-      type: 'boolean'
+module.exports = function defineBuildCommand (args) {
+  args.command({
+    command: 'build',
+    describe: 'Builds the browser and server JS bundles',
+    builder: {
+      static: {
+        alias: 's',
+        default: false,
+        describe: 'Statically build locations',
+        type: 'boolean'
+      },
+      clean: {
+        alias: 'c',
+        default: true,
+        describe: 'Clean up artifacts in build / cache directories before ' +
+          'building',
+        type: 'boolean'
+      },
+      production: {
+        alias: 'p',
+        default: false,
+        describe: 'Minifies the output, generates source maps and removes ' +
+          'React developer warnings',
+        type: 'boolean'
+      }
     },
-    clean: {
-      alias: 'c',
-      default: true,
-      describe: 'Clean up artifacts in build / cache directories before ' +
-        'building',
-      type: 'boolean'
-    },
-    production: {
-      alias: 'p',
-      default: false,
-      describe: 'Minifies the output, generates source maps and removes ' +
-        'React developer warnings',
-      type: 'boolean'
+    handler: function buildHandler (argv) {
+      if (argv.production) {
+        process.env.NODE_ENV = 'production';
+      }
+      process.env.HOPS_MODE = argv.static ? 'static' : 'dynamic';
+      hopsBuild.runBuild(argv);
     }
-  },
-  handler: function buildHandler (argv) {
-    if (argv.production) {
-      process.env.NODE_ENV = 'production';
-    }
-    process.env.HOPS_MODE = argv.static ? 'static' : 'dynamic';
-    hopsBuild.runBuild(argv);
-  }
+  });
 };
 
 if (require.main === module) {
-  require('hops-local-cli').run(module.exports);
+  require('hops-local-cli').run(module.exports, 'build');
 }
--- packages/build/commands/develop.js
+++ packages/build/commands/develop.js
@@ -3,31 +3,33 @@
 
 var hopsBuild = require('..');
 
-module.exports = {
-  command: 'develop',
-  describe: 'Starts a webpack-dev-server to enable local development with ' +
-    'hot code reloading',
-  builder: {
-    static: {
-      alias: 's',
-      default: false,
-      describe: 'Serve app in hot mode with static env variable turned on',
-      type: 'boolean'
+module.exports = function defineDevelopCommand (args) {
+  args.command({
+    command: 'develop',
+    describe: 'Starts a webpack-dev-server to enable local development with ' +
+      'hot code reloading',
+    builder: {
+      static: {
+        alias: 's',
+        default: false,
+        describe: 'Serve app in hot mode with static env variable turned on',
+        type: 'boolean'
+      },
+      clean: {
+        alias: 'c',
+        default: true,
+        describe: 'Clean up artifacts in build / cache directories before ' +
+          'building',
+        type: 'boolean'
+      }
     },
-    clean: {
-      alias: 'c',
-      default: true,
-      describe: 'Clean up artifacts in build / cache directories before ' +
-        'building',
-      type: 'boolean'
+    handler: function developHandler (argv) {
+      process.env.HOPS_MODE = argv.static ? 'static' : 'dynamic';
+      hopsBuild.runServer(argv);
     }
-  },
-  handler: function developHandler (argv) {
-    process.env.HOPS_MODE = argv.static ? 'static' : 'dynamic';
-    hopsBuild.runServer(argv);
-  }
+  });
 };
 
 if (require.main === module) {
-  require('hops-local-cli').run(module.exports);
+  require('hops-local-cli').run(module.exports, 'develop');
 }
