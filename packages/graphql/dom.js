8c3a7fc90c9778ad157ab41cfc2f5a2951dcc143
Daniel Dembach <daniel@dmbch.net>
2017-10-24 22:10:28 +0200

    feat(graphql): upgrade to apollo-client 2.0
    upgrade to latest release candidate of apollo-client that does not rely on redux any more

--- packages/graphql/dom.js
+++ packages/graphql/dom.js
@@ -1,8 +1,14 @@
+'use strict';
 
 var common = require('./lib/common');
 
 exports.Context = exports.createContext = common.Context.extend({
+  createCache: function () {
+    return common.Context.prototype.createCache.call(this).restore(
+      global[common.APOLLO_STATE]
+    );
+  },
   getIntrospectionResult: function () {
-    return global[common.INTROSPECTION_RESULT];
+    return global[common.APOLLO_IQRD];
   }
 });
