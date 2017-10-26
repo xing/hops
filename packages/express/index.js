9e7523a67fba29030f41f8b76f1f4cdbf7b46ed5
Daniel Dembach <daniel@dmbch.net>
2017-10-26 18:02:47 +0200

    fix(express): re-introduce exported `startServer` method

--- packages/express/index.js
+++ packages/express/index.js
@@ -3,9 +3,12 @@
 var server = require('hops-server');
 var createApp = require('./app');
 
+function runServer (options, callback) {
+  server.run(createApp(), callback);
+}
+
 module.exports = {
   createApp: createApp,
-  runServer: function runServer (options, callback) {
-    server.run(createApp(), callback);
-  }
+  runServer: runServer,
+  startServer: runServer
 };
