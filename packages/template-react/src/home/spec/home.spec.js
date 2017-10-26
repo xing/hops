674f39f9760a279045a0ad55f2b0c460c493e01f
Daniel Dembach <daniel@dmbch.net>
2017-10-26 13:11:58 +0200

    test(template-graphql): add missing example test case for loaded state

--- packages/template-react/src/home/spec/home.spec.js
+++ packages/template-react/src/home/spec/home.spec.js
@@ -4,9 +4,30 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { Home } from '../home';
 
-it('renders correctly', () => {
+it('renders loading state correctly', () => {
   const tree = renderer.create(
     <Home data={{ loading: true }} />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders loaded state correctly', () => {
+  const data = {
+    loading: false,
+    github: {
+      repo: {
+        commits: [{
+          sha: 'foo',
+          message: 'bar',
+          author: {
+            login: 'baz'
+          }
+        }]
+      }
+    }
+  };
+  const tree = renderer.create(
+    <Home data={ data } />
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
