3a575b1febecae5c5ed6ee8b528bfc038942c159
Daniel Dembach <daniel@dmbch.net>
2017-11-06 23:10:34 +0100

    feat(react): introduce mixin support
    switch to mixinable instead of custom extension mechanism
    
    BREAKING CHANGE: Context.extends is removed, Context.mixin is added

--- packages/react/lib/common.js
+++ packages/react/lib/common.js
@@ -1,25 +1,14 @@
 'use strict';
 
-exports.Context = function Context (options) {
-  this.options = options || {};
-  this.initialize(this.options);
-};
+var mixin = require('mixinable');
 
-exports.Context.extend = function (protoExtension, staticExtension) {
-  var parent = this;
-  var Context = function (options) {
-    if (!(this instanceof Context)) {
-      return new Context(options);
-    }
-    parent.call(this, options);
-  };
-  return Object.assign(Context, parent, staticExtension, {
-    prototype: Object.assign(
-      Object.create(parent.prototype),
-      protoExtension,
-      {
-        constructor: Context
-      }
-    )
-  });
-};
+exports.Context = mixin({
+  constructor: function (options) {
+    this.options = options || {};
+    this.initialize(this.options);
+  },
+  initialize: mixin.sequence(),
+  bootstrap: mixin.parallel(Promise.resolve.bind(Promise)),
+  enhanceElement: mixin.pipe(Promise.resolve.bind(Promise)),
+  getTemplateData: mixin.pipe()
+});
