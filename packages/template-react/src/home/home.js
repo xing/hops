4533445b8e852397f7b7b533a1501f515ed52f8f
ZauberNerd <zaubernerd@zaubernerd.de>
2017-11-22 10:45:45 +0100

    refactor(template-react): remove graphql from default template
    This commit removes the `hops-graphql` support from the default
    `hops-template-react`.
    
    BREAKING CHANGE:
    `hops-template-react` (default template) now no longer supports graphql
    out of the box. You can add the support yourself again or use a
    different template (we will provide a new `hops-template-graphql`
    shortly).
    
    Closes: #276

--- packages/template-react/src/home/home.js
+++ packages/template-react/src/home/home.js
@@ -1,31 +1,19 @@
 // @flow
 import React from 'react';
 import Helmet from 'react-helmet';
-import { graphql } from 'react-apollo';
 
 import styles from './styles.css';
-import query from './commits.gql';
 
-export const withCommits = graphql(query);
 
-export const Home = ({ data: { loading, github } }) => (
-  <div>
-    <Helmet>
-      <title>Hops Demo</title>
-    </Helmet>
-    <h1 className={styles.headline}>
-      Hello World!
-    </h1>
-    {
-      (loading)
-        ? (<p>Loading...</p>)
-        : (github.repo.commits.map(commit => (
-          <p key={commit.sha}>
-            "{commit.message}" by <b>{commit.author.login}</b>
-          </p>
-        )))
-    }
-  </div>
-);
-
-export default withCommits(Home);
+export default function Home () {
+  return (
+    <div>
+      <Helmet>
+        <title>Hops Demo</title>
+      </Helmet>
+      <h1 className={styles.headline}>
+        Hello World!
+      </h1>
+    </div>
+  );
+}
