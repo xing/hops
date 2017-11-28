213eb0509614c9140c2f24ae0ec9662d27ea0724
Robin Drexler <robin.drexler@xing.com>
2017-11-28 11:35:10 +0100

    test(hops-redux): add test for middleware option

---
+++ packages/spec/redux.js
@@ -0,0 +1,42 @@
+/* eslint-env node, mocha */
+var assert = require('assert');
+var React = require('react');
+
+var redux = require('hops-redux');
+var hopsReact = require('hops-react');
+
+describe('redux', function () {
+  var defaultOptions = {
+    someSlice: function () {
+      return null;
+    }
+  };
+
+  function setupContext (options) {
+    return hopsReact.combineContexts(redux.contextDefinition)(
+      Object.assign({}, defaultOptions, options || {})
+    );
+  }
+
+  it('allows to set middlewares via option', function () {
+    var called = false;
+    var context = setupContext({
+      middlewares: [
+        function () {
+          return function () {
+            return function () {
+              called = true;
+            };
+          };
+        }
+      ]
+    });
+
+    return context
+      .enhanceElement(React.createElement('span'))
+      .then(function (element) {
+        element.props.store.dispatch({ type: '' });
+        assert.ok(called);
+      });
+  });
+});
