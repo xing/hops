0a65138e4897a1f42910114cc470ee6be59c688c
ZauberNerd <zaubernerd@zaubernerd.de>
2017-12-13 13:45:31 +0100

    feat(template-graphql): Add grapqhl example template
    Closes: #304

---
+++ packages/template-graphql/src/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
+
+import { render, Miss } from 'hops-react';
+import { createContext } from 'hops-graphql';
+
+import { Home } from './home';
+
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Miss />
+  </Switch>
+);
+
+export default render(<App />, createContext());
