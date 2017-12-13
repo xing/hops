98b1c791c1528091e6667e0b0dd0717b49615e32
ZauberNerd <zaubernerd@zaubernerd.de>
2017-12-13 16:55:35 +0100

    refactor(graphql): use getTemplateData to build query from tree

--- packages/graphql/node.js
+++ packages/graphql/node.js
@@ -12,12 +12,6 @@ exports.GraphQLContext = function() {
   return common.constructor.apply(this, arguments);
 };
 exports.GraphQLContext.prototype = Object.assign({}, common, {
-  enhanceElement: function(reactElement) {
-    var enhancedElement = common.enhanceElement.call(this, reactElement);
-    return ReactApollo.getDataFromTree(enhancedElement).then(function() {
-      return enhancedElement;
-    });
-  },
   enhanceClientOptions: function(options) {
     return Object.assign(common.enhanceClientOptions.call(this, options), {
       ssrMode: true,
@@ -26,19 +20,23 @@ exports.GraphQLContext.prototype = Object.assign({}, common, {
   getIntrospectionResult: function() {
     return introspectionResult;
   },
-  getTemplateData: function(templateData) {
-    return Object.assign({}, templateData, {
-      globals: (templateData.globals || []).concat([
-        {
-          name: constants.APOLLO_IQRD,
-          value: this.getIntrospectionResult(),
-        },
-        {
-          name: constants.APOLLO_STATE,
-          value: this.client.cache.extract(),
-        },
-      ]),
-    });
+  getTemplateData: function(templateData, rootElement) {
+    return ReactApollo.getDataFromTree(rootElement).then(
+      function() {
+        return Object.assign({}, templateData, {
+          globals: (templateData.globals || []).concat([
+            {
+              name: constants.APOLLO_IQRD,
+              value: this.getIntrospectionResult(),
+            },
+            {
+              name: constants.APOLLO_STATE,
+              value: this.client.cache.extract(),
+            },
+          ]),
+        });
+      }.bind(this)
+    );
   },
 });
 
