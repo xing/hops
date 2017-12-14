ebbd9307aa0ae42f84a631fb8155a78b5987b576
Daniel Dembach <daniel@dmbch.net>
2017-12-15 00:20:22 +0100

    feat(build-config): re-add config to node build
    try and make sure absolute paths are always correct

--- packages/build-config/loaders/config.js
+++ packages/build-config/loaders/config.js
@@ -1,18 +1,47 @@
 'use strict';
 
+var loaderUtils = require('loader-utils');
+
 var hopsConfig = require('hops-config');
 
-function removeAbsolutePaths(config) {
-  return Object.keys(config).reduce(function(browserConfig, key) {
-    if (!/(config|file|dir)s?$/i.test(key)) {
-      browserConfig[key] = config[key];
-    }
+function getConfig() {
+  return (
+    'module.exports = ' +
+    JSON.stringify(
+      Object.assign(
+        {},
+        hopsConfig,
+        Object.keys(hopsConfig)
+          .filter(function(key) {
+            return /(config|file|dir)s?$/i.test(key);
+          })
+          .reduce(function(result, key) {
+            result[key] = (function shorten(item) {
+              if (typeof item === 'string') {
+                return item.replace(new RegExp(hopsConfig.appDir), '.');
+              } else if (Array.isArray(item)) {
+                return item.map(shorten);
+              } else {
+                return item;
+              }
+            })(hopsConfig[key]);
+            return result;
+          }, {})
+      )
+    )
+  );
+}
 
-    return browserConfig;
-  }, {});
+function getNodeConfig() {
+  return [
+    'var path = require("path");',
+    'var expand = path.join.bind(path, process.cwd());',
+    getConfig().replace(/(config|file|dir)":"(\.[^"]+)"/gi, '$1":expand("$2")'),
+  ].join('');
 }
 
 module.exports = function() {
   this.cacheable();
-  return 'module.exports = ' + JSON.stringify(removeAbsolutePaths(hopsConfig));
+  var options = loaderUtils.getOptions(this);
+  return options && options.target === 'node' ? getNodeConfig() : getConfig();
 };
