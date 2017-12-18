29a5f61eafe9d2145beac58b1a7119bf4bede24d
Robin Drexler <robin.drexler@xing.com>
2017-12-18 14:06:43 +0100

    fix(hops-build-config): exclude manifest.js.map as well

--- packages/build-config/configs/build.js
+++ packages/build-config/configs/build.js
@@ -29,7 +29,7 @@ module.exports = {
   devtool: 'source-map',
   plugins: [
     new StatsWriterPlugin({ fields: null }),
-    new WriteFilePlugin(/^manifest\.js?$/),
+    new WriteFilePlugin(/^manifest\.js(\.map)?$/),
     new WriteManifestPlugin(),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
