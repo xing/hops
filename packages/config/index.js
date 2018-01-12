d9f8c15bf516dceab3c94120e3681b9cdbb86acd
Daniel Dembach <daniel@dmbch.net>
2018-01-12 15:15:02 +0100

    feat(config): introduce cosmiconfig
    Replace custom, npm based config mechanism and switch to cosmiconfig, a
    well used open source library.
    
    BREAKING CHANGE: hops-config no longer supports the previous, npm based
    config mechanism. hops now needs to be configured using a `hops` field
    in `package.json` instead of `config.hops`. Local config overrides
    (using npm) are no longer supported.

--- packages/config/index.js
+++ packages/config/index.js
@@ -4,45 +4,62 @@ var path = require('path');
 
 var root = require('pkg-dir').sync(process.cwd());
 
-var parseEnv = require('./lib/parse-env');
+var explorer = require('cosmiconfig')('hops', {
+  rcExtensions: true,
+  stopDir: root,
+  sync: true,
+});
 
-function getNpmConfig() {
-  var npmConfig = parseEnv('hops');
-  if (!Object.keys(npmConfig).length) {
-    try {
-      require.resolve(path.join(root, 'package.json'));
-      npmConfig = require(path.join(root, 'package.json')).config.hops;
-    } catch (_) {}
-  }
-  return npmConfig;
+function getRawConfig() {
+  var result = explorer.load(process.cwd());
+  return Object.assign(
+    {
+      https: false,
+      host: '0.0.0.0',
+      port: 8080,
+      locations: [],
+      basePath: '',
+      assetPath: '',
+      browsers: '> 1%, last 2 versions, Firefox ESR',
+      node: 'current',
+      envVars: { HOPS_MODE: 'dynamic' },
+      moduleDirs: [],
+      appDir: '.',
+      buildDir: 'build',
+      cacheDir: 'node_modules/.cache/hops',
+    },
+    result ? result.config : {}
+  );
 }
 
-function extendConfig(config) {
-  var npmConfig = getNpmConfig();
-  if (npmConfig.extends) {
+function applyExtension(config) {
+  var result = Object.assign({}, config);
+  if (config.extends) {
     try {
-      require.resolve(npmConfig.extends);
-      Object.assign(config, require(npmConfig.extends));
+      require.resolve(config.extends);
+      Object.assign(result, require(config.extends));
     } catch (_) {
-      Object.assign(config, require(path.join(root, npmConfig.extends)));
+      Object.assign(result, require(path.join(root, config.extends)));
     }
   }
-  Object.assign(
-    config,
-    npmConfig,
-    npmConfig.env && npmConfig.env[process.env.NODE_ENV]
-  );
-  delete config.env;
-  return config;
+  return result;
+}
+
+function applyEnvironment(config) {
+  var env = process.env.HOPS_ENV || process.env.NODE_ENV;
+  var result = Object.assign({}, config, config.env && config.env[env]);
+  delete result.env;
+  return result;
 }
 
 function resolvePaths(config) {
+  var result = Object.assign({}, config);
   Object.keys(config)
     .filter(function(key) {
       return /(config|file|dir)s?$/i.test(key);
     })
     .forEach(function(key) {
-      config[key] = (function resolve(item) {
+      result[key] = (function resolve(item) {
         if (typeof item === 'string') {
           if (item.indexOf('<') === 0) {
             item = item.replace(/^(?:<([^>]+)>)(.*)/, function() {
@@ -57,13 +74,13 @@ function resolvePaths(config) {
         }
       })(config[key]);
     });
-  return config;
+  return result;
 }
 
 function normalizeURLs(config) {
   var basePath = config.basePath.replace(/^\/*/, '/').replace(/\/*$/, '');
   var assetPath = config.assetPath.replace(/(^\/*|\/*$)/g, '');
-  return Object.assign(config, {
+  return Object.assign({}, config, {
     locations: config.locations
       .map(function(location) {
         return basePath + location.replace(/\/*$/, '').replace(/^\/*/, '/');
@@ -76,23 +93,12 @@ function normalizeURLs(config) {
   });
 }
 
-module.exports = [extendConfig, resolvePaths, normalizeURLs].reduce(
-  function(result, step) {
-    return step(Object.assign({}, result));
-  },
-  {
-    https: false,
-    host: '0.0.0.0',
-    port: 8080,
-    locations: [],
-    basePath: '',
-    assetPath: '',
-    browsers: '> 1%, last 2 versions, Firefox ESR',
-    node: 'current',
-    envVars: { HOPS_MODE: 'dynamic' },
-    moduleDirs: [],
-    appDir: '.',
-    buildDir: 'build',
-    cacheDir: 'node_modules/.cache/hops',
-  }
-);
+module.exports = [
+  getRawConfig,
+  applyExtension,
+  applyEnvironment,
+  resolvePaths,
+  normalizeURLs,
+].reduce(function(result, step) {
+  return step(result);
+}, null);
