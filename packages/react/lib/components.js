f960b4f3578ed3ef496c49dc4c69634c13d6485a
Robin Drexler <drexler.robin@gmail.com>
2018-01-24 18:57:45 +0100

    feat(hops-react): add header component to allow setting http headers declaratively

--- packages/react/lib/components.js
+++ packages/react/lib/components.js
@@ -15,3 +15,11 @@ exports.Status = ReactRouter.withRouter(function Status(props) {
   }
   return null;
 });
+
+exports.Header = ReactRouter.withRouter(function Header(props) {
+  if (props.staticContext) {
+    props.staticContext.headers = props.staticContext.headers || {};
+    props.staticContext.headers[props.name] = props.value;
+  }
+  return null;
+});
