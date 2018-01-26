5de272d71b18aba841198cacf0fe1e51f9b045ab
dmbch <daniel@dmbch.net>
2018-01-26 22:52:41 +0100

    feat(build-config): update webpack to v4
    fix configs, temporarily remove extract-text-webpack-plugin

--- packages/build-config/configs/build.js
+++ packages/build-config/configs/build.js
@@ -3,7 +3,6 @@
 var path = require('path');
 
 var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var UglifyPlugin = require('uglifyjs-webpack-plugin');
 var StatsWriterPlugin = require('webpack-stats-plugin').StatsWriterPlugin;
 var WriteManifestPlugin = require('../plugins/write-manifest');
@@ -14,12 +13,13 @@ var hopsConfig = require('hops-config');
 var getAssetPath = path.join.bind(path, hopsConfig.assetPath);
 
 module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   entry: require.resolve('../shims/build'),
   output: {
     path: hopsConfig.buildDir,
     publicPath: '/',
     filename: getAssetPath('[name]-[chunkhash:16].js'),
-    chunkFilename: getAssetPath('chunk-[id]-[chunkhash:16].js'),
+    chunkFilename: getAssetPath('[name]-[chunkhash:16].js'),
   },
   context: hopsConfig.appDir,
   resolve: require('../sections/resolve')('build'),
@@ -27,35 +27,34 @@ module.exports = {
     rules: require('../sections/module-rules')('build'),
   },
   devtool: 'source-map',
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: function(module) {
+            if (module.resource) {
+              if (
+                module.resource.indexOf('hops-config') > -1 ||
+                module.resource.indexOf('.css') === module.resource.length - 4
+              ) {
+                return false;
+              }
+            }
+            return (
+              module.context && module.context.indexOf('node_modules') > -1
+            );
+          },
+          name: 'vendor',
+          chunks: 'all',
+        },
+      },
+    },
+  },
   plugins: [
     new WriteFilePlugin(/^manifest\.js(\.map)?$/),
     new WriteManifestPlugin(),
     new StatsWriterPlugin({ fields: null }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: function(module) {
-        if (module.resource) {
-          if (
-            module.resource.indexOf('hops-config') > -1 ||
-            module.resource.indexOf('.css') === module.resource.length - 4
-          ) {
-            return false;
-          }
-        }
-        return module.context && module.context.indexOf('node_modules') > -1;
-      },
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      filename: 'manifest.js',
-      minChunks: Infinity,
-    }),
     new webpack.HashedModuleIdsPlugin(),
-    new ExtractTextPlugin({
-      filename: getAssetPath('[name]-[contenthash:16].css'),
-      allChunks: true,
-      ignoreOrder: true,
-    }),
     new webpack.EnvironmentPlugin(
       Object.assign(
         {
