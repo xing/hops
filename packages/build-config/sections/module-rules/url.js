36cc2db55df330e5d99f5e971a0597a6b1a13a09
dmbch <daniel@dmbch.net>
2018-02-03 23:42:59 +0100

    refactor(build-config): simplify, cleanup module rules

--- packages/build-config/sections/module-rules/url.js
+++ packages/build-config/sections/module-rules/url.js
@@ -6,11 +6,9 @@ var hopsConfig = require('hops-config');
 
 exports.default = {
   test: /\.(png|gif|jpe?g|webp)$/,
-  use: {
-    loader: require.resolve('url-loader'),
-    options: {
-      limit: 10000,
-      name: path.join(hopsConfig.assetPath, '[name]-[hash:16].[ext]'),
-    },
+  loader: require.resolve('url-loader'),
+  options: {
+    limit: 10000,
+    name: path.join(hopsConfig.assetPath, '[name]-[hash:16].[ext]'),
   },
 };
