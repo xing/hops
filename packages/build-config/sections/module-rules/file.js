5b1142d68b17b8973841d7b05567aaa632ad9942
ZauberNerd <zaubernerd@zaubernerd.de>
2018-02-18 12:49:25 +0100

    refactor(build-config): do not emit files during server build

--- packages/build-config/sections/module-rules/file.js
+++ packages/build-config/sections/module-rules/file.js
@@ -11,3 +11,12 @@ exports.default = {
     name: path.join(hopsConfig.assetPath, '[name]-[hash:16].[ext]'),
   },
 };
+
+exports.node = {
+  exclude: /\.(?:m?jsx?|html|json)$/,
+  loader: require.resolve('file-loader'),
+  options: {
+    name: path.join(hopsConfig.assetPath, '[name]-[hash:16].[ext]'),
+    emitFile: false,
+  },
+};
