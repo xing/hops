c680cb3b33a012b6269e12cbc4f467a627f7ed80
Robin Drexler <drexler.robin@gmail.com>
2018-03-12 21:52:12 +0100

    test(react): add tests for Header component

--- packages/spec/mock/integration/main.js
+++ packages/spec/mock/integration/main.js
@@ -5,10 +5,12 @@ import { Miss, render, createContext } from 'hops-react';
 import { headline } from './style.css';
 
 const Home = () => <h1 className={headline}>Hello World!</h1>;
+import Headers from './headers';
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route path="/headers" component={Headers} />
     <Miss />
   </Switch>
 );
