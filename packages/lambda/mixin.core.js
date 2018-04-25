684b582495ab4347019104a3087af834c626056b
ZauberNerd <zaubernerd@zaubernerd.de>
2018-04-25 15:09:23 +0200

    refactor(lambda): use registerCommands to register lambda commands

---
+++ packages/lambda/mixin.core.js
@@ -0,0 +1,49 @@
+const { Mixin } = require('@untool/core');
+const strip = require('strip-indent');
+
+class LambdaMixin extends Mixin {
+  registerCommands(yargs) {
+    return yargs.command('lambda', 'manage your lambda deployment', yargs =>
+      yargs
+        .usage('Usage: hops lambda <command>')
+        .command({
+          command: 'deploy',
+          describe: 'Deploys your hops application to AWS lambda',
+          handler: require('./index').deploy,
+        })
+        .command({
+          command: 'destroy',
+          describe:
+            'Delete all AWS resources created by this hops application.',
+          builder: {
+            'keep-bucket': {
+              type: 'boolean',
+              default: false,
+              describe: strip(`
+                Set this to true if you want to delete all files in
+                the S3 bucket but keep the bucket itself.
+              `),
+            },
+            'keep-files': {
+              type: 'boolean',
+              default: false,
+              describe: strip(`
+                Set this to true if you want to keep all files in the
+                S3 bucket.
+              `),
+            },
+            yes: {
+              type: 'boolean',
+              default: false,
+              describe: "Don't ask for confirmation.",
+            },
+          },
+          handler: require('./index').destroy,
+        })
+        .help('help')
+        .demandCommand()
+    );
+  }
+}
+
+module.exports = LambdaMixin;
