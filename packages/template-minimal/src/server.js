24361779f6c8c968268d9f77e6e591a096851669
ZauberNerd <zaubernerd@zaubernerd.de>
2018-05-09 14:03:17 +0200

    fix(template-minimal): remove manifest, use assets from res.locals

--- packages/template-minimal/src/server.js
+++ packages/template-minimal/src/server.js
@@ -1,19 +1,18 @@
-import hopsConfig from 'hops-config';
-
 export default (req, res, next) => {
   res.send(
     `<!doctype html>
 <html charset="utf-8">
   <head>
       <title>Hello Hops</title>
-      ${hopsConfig.assets.css
+      ${res.locals.hops.assets.css
         .map(css => `<link rel="stylesheet" href="${css}"></link>`)
         .join('')}
   </head>
   <body>
     <div id="app"></app>
-    <script>${hopsConfig.manifest}</script>
-    ${hopsConfig.assets.js.map(js => `<script src="${js}"></script>`).join('')}
+    ${res.locals.hops.assets.js
+      .map(js => `<script src="${js}"></script>`)
+      .join('')}
   </body>
 </html>
 `
