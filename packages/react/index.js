424e91abf0fdaa4db4b5580d7e63f4a1970a2c79
ZauberNerd <zaubernerd@zaubernerd.de>
2018-05-14 15:09:50 +0200

    refactor(react): use untool react mixins

---
+++ packages/react/index.js
@@ -0,0 +1,9 @@
+const { render } = require('@untool/core/lib/runtime');
+const { Header, Miss, Status } = require('@untool/react/lib/components');
+
+module.exports = {
+  Header,
+  Miss,
+  Status,
+  render,
+};
---
+++ packages/react/mixin.core.js
@@ -0,0 +1,1 @@
+module.exports = require('@untool/react/mixin.core');
