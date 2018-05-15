6f23b452cdb1cfebf46e519b75cb48635b9f67bb
ZauberNerd <zaubernerd@zaubernerd.de>
2018-05-15 10:48:31 +0200

    refactor(jest-preset): include babel config in preset directly

--- packages/jest-preset/transforms/babel.js
+++ packages/jest-preset/transforms/babel.js
@@ -1,15 +1,21 @@
 var babelJest = require('babel-jest');
 
-var loaderConfig = require('hops-build-config/sections/module-rules/babel');
-
-var babelConfig = loaderConfig.node.options;
-
-var presetEnvConfig = babelConfig.presets[0][1];
-
-presetEnvConfig.modules = 'commonjs';
-
 module.exports = babelJest.createTransformer({
-  presets: babelConfig.presets,
-  plugins: babelConfig.plugins,
+  presets: [
+    [
+      'env',
+      {
+        modules: 'commonjs',
+        useBuiltIns: true,
+        targets: { node: 'current' },
+      },
+    ],
+    'react',
+  ],
+  plugins: [
+    require.resolve('babel-plugin-dynamic-import-node'),
+    require.resolve('babel-plugin-transform-class-properties'),
+    require.resolve('babel-plugin-transform-object-rest-spread'),
+  ],
   babelrc: false,
 });
