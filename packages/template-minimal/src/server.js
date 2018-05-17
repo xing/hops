4d02eea25d45c3c65e71bee6572fe114e97cacc3
ZauberNerd <zaubernerd@zaubernerd.de>
2018-05-17 12:22:08 +0200

    fix(template-minimal): adapt to new render method signature

--- packages/template-minimal/src/server.js
+++ packages/template-minimal/src/server.js
@@ -1,16 +1,16 @@
-export default (req, res, next) => {
+export default config => (req, res, next) => {
   res.send(
     `<!doctype html>
 <html charset="utf-8">
   <head>
       <title>Hello Hops</title>
-      ${res.locals.hops.assets.css
+      ${res.locals.assetsByType.css
         .map(css => `<link rel="stylesheet" href="${css}"></link>`)
         .join('')}
   </head>
   <body>
     <div id="app"></app>
-    ${res.locals.hops.assets.js
+    ${res.locals.assetsByType.js
       .map(js => `<script src="${js}"></script>`)
       .join('')}
   </body>
