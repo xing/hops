7e79abd756827dec8f618184f7990abf13551b73
ZauberNerd <zaubernerd@zaubernerd.de>
2018-06-19 17:33:35 +0200

    feat(postcss): implement postcss package

---
+++ packages/postcss/mixin.core.js
@@ -0,0 +1,129 @@
+const postcssImportPlugin = require('postcss-import');
+const postcssPresetEnv = require('postcss-preset-env');
+const ExtractCSSPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
+const { Mixin } = require('@untool/core');
+const {
+  uri: { resolveRelative },
+} = require('@untool/express');
+
+const cssLoaderOptions = {
+  camelCase: true,
+  modules: true,
+  localIdentName: '[folder]-[name]-[local]-[hash:8]',
+  sourceMap: process.env.NODE_ENV !== 'production',
+};
+
+const getCSSLoaderConfig = browsers => ({
+  test: [/\.css$/],
+  use: [
+    {
+      loader: require.resolve('css-loader'),
+      options: {
+        ...cssLoaderOptions,
+        importLoaders: 1,
+      },
+    },
+    {
+      loader: require.resolve('postcss-loader'),
+      options: {
+        ident: 'postcss',
+        plugins: [
+          postcssImportPlugin(),
+          postcssPresetEnv({
+            browsers,
+          }),
+        ],
+      },
+    },
+  ],
+});
+
+class PostCSSMixin extends Mixin {
+  configureWebpack(webpackConfig, loaderConfigs, target) {
+    switch (target) {
+      case 'build':
+        this.configureBuild(webpackConfig, loaderConfigs);
+        break;
+      case 'develop':
+        this.configureDevelop(loaderConfigs);
+        break;
+      case 'node':
+        this.configureNode(loaderConfigs);
+        break;
+    }
+    return webpackConfig;
+  }
+
+  configureBuild(webpackConfig, { allLoaderConfigs, jsLoaderConfig }) {
+    const loaderConfig = {
+      ...getCSSLoaderConfig(this.config.browsers),
+      use: [
+        ExtractCSSPlugin.loader,
+        ...getCSSLoaderConfig(this.config.browsers).use,
+      ],
+    };
+
+    allLoaderConfigs.splice(
+      allLoaderConfigs.indexOf(jsLoaderConfig),
+      0,
+      loaderConfig
+    );
+
+    webpackConfig.plugins.push(
+      new ExtractCSSPlugin({
+        filename: resolveRelative(
+          this.config.assetPath,
+          '[name]-[contenthash:12].css'
+        ),
+        chunkFilename: resolveRelative(
+          this.config.assetPath,
+          '[name]-[contenthash:12].css'
+        ),
+      })
+    );
+
+    webpackConfig.optimization.minimizer.push(
+      new OptimizeCSSPlugin({
+        cssProcessorOptions: {
+          reduceIdents: { disable: true },
+          zindex: { disable: true },
+          mergeIdents: { disable: true },
+          discardUnused: { disable: true },
+        },
+      })
+    );
+  }
+
+  configureDevelop({ allLoaderConfigs, jsLoaderConfig }) {
+    const loaderConfig = {
+      ...getCSSLoaderConfig(this.config.browsers),
+      use: [
+        require.resolve('style-loader'),
+        ...getCSSLoaderConfig(this.config.browsers).use,
+      ],
+    };
+
+    allLoaderConfigs.splice(
+      allLoaderConfigs.indexOf(jsLoaderConfig),
+      0,
+      loaderConfig
+    );
+  }
+
+  configureNode({ allLoaderConfigs, jsLoaderConfig }) {
+    const cssLoaderConfig = {
+      test: [/\.css$/],
+      loader: require.resolve('css-loader/locals'),
+      options: cssLoaderOptions,
+    };
+
+    allLoaderConfigs.splice(
+      allLoaderConfigs.indexOf(jsLoaderConfig),
+      0,
+      cssLoaderConfig
+    );
+  }
+}
+
+module.exports = PostCSSMixin;
