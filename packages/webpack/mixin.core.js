fbe902b574274cd73c422d4cffeace6b69a9ac94
ZauberNerd <zaubernerd@zaubernerd.de>
2018-06-20 12:10:29 +0200

    feat(webpack): print compilation stats and watch runs

---
+++ packages/webpack/mixin.core.js
@@ -0,0 +1,37 @@
+const { Mixin } = require('@untool/core');
+const prettyMs = require('pretty-ms');
+
+class LoggerPlugin {
+  apply(compiler) {
+    compiler.hooks.done.tap('hops-logger-plugin', stats => {
+      if (stats.compilation.name === 'develop') {
+        console.log(
+          'Compiled successfully',
+          'in',
+          prettyMs(stats.endTime - stats.startTime)
+        );
+      }
+    });
+  }
+}
+
+class WebpackCoreMixin extends Mixin {
+  configureWebpack(webpackConfig) {
+    webpackConfig.plugins.push(new LoggerPlugin());
+    return webpackConfig;
+  }
+
+  inspectBuild(stats) {
+    console.log(
+      stats.toString({
+        colors: true,
+        hash: false,
+        modules: false,
+        entrypoints: false,
+        chunks: false,
+      })
+    );
+  }
+}
+
+module.exports = WebpackCoreMixin;
