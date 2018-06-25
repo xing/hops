136f464cef5039bbf0c6a834d3b5744377b76464
ZauberNerd <zaubernerd@zaubernerd.de>
2018-06-25 15:49:21 +0200

    fix(react): do not fail if _hopsEnvironmentVariables is not defined

--- packages/react/index.js
+++ packages/react/index.js
@@ -2,7 +2,9 @@ const { render } = require('@untool/core');
 const { Header, Miss, Status, withCLIArguments } = require('./lib/components');
 
 const getEnvironmentVariable = name =>
-  process.env[name] || global._hopsEnvironmentVariables[name];
+  process.env[name] || global._hopsEnvironmentVariables
+    ? global._hopsEnvironmentVariables[name]
+    : undefined;
 
 module.exports = {
   Header,
