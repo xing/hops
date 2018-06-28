df00ec6f6aff67879e2406f248b9a6a2706bcef3
ZauberNerd <zaubernerd@zaubernerd.de>
2018-06-28 17:13:18 +0200

    chore: use recommended linter settings and fix resulting issues

--- packages/graphql/__tests__/node.spec.js
+++ packages/graphql/__tests__/node.spec.js
@@ -1,4 +1,5 @@
 /* eslint-env node, jest */
+/* eslint-disable */
 
 var React = require('react');
 
--- packages/jest-preset/transforms/graphql.js
+++ packages/jest-preset/transforms/graphql.js
@@ -1,6 +1,10 @@
 function getLoader() {
   try {
     require.resolve('graphql-tag/loader');
+    // Not all users of the jest-preset-hops package want to use graphql too
+    // therefore we only want to add the graphql-tag/loader if it is already
+    // installed in the application
+    /* eslint-disable-next-line node/no-extraneous-require */
     return require('graphql-tag/loader');
   } catch (_) {
     console.warn('graphql-tag/loader not found');
