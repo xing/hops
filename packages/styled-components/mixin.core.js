621e2e39156fd88b61aa7a7d065c2af1fbccc18e
ZauberNerd <zaubernerd@zaubernerd.de>
2018-06-28 11:12:19 +0200

    feat(styled-components): introduce package

---
+++ packages/styled-components/mixin.core.js
@@ -0,0 +1,18 @@
+const { Mixin } = require('@untool/core');
+
+class StyledComponentsMixin extends Mixin {
+  configureBuild(webpackConfig, { jsLoaderConfig }, target) {
+    jsLoaderConfig.options.plugins.unshift([
+      require.resolve('babel-plugin-styled-components'),
+      {
+        ssr: true,
+        displayName: target !== 'build',
+        fileName: target !== 'build',
+      },
+    ]);
+
+    return webpackConfig;
+  }
+}
+
+module.exports = StyledComponentsMixin;
