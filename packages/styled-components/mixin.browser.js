9f9b98b5a271b94c6bca40e54a68ead2e0abb971
ZauberNerd <zaubernerd@zaubernerd.de>
2018-06-28 17:54:39 +0200

    refactor(styled-components): unify render options handling

--- packages/styled-components/mixin.browser.js
+++ packages/styled-components/mixin.browser.js
@@ -3,9 +3,9 @@ const React = require('react');
 const { ThemeProvider } = require('styled-components');
 
 class StyledComponentsMixin extends Mixin {
-  constructor(config, element, { styled } = {}) {
+  constructor(config, element, { styled: options = {} } = {}) {
     super(config);
-    this.theme = styled ? styled.theme : {};
+    this.theme = options.theme || {};
   }
 
   enhanceElement(reactElement) {
