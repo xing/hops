8911f940d01b17a6986f09d8b48e855540f64eb5
ZauberNerd <zaubernerd@zaubernerd.de>
2018-07-10 16:50:14 +0200

    test: add basic pwa test

---
+++ packages/spec/integration/pwa/service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'hops-pwa-cache';
+
+export default config => assets => {
+  const cacheables = assets.map(a => '/' + a).concat(config.locations);
+
+  self.addEventListener('install', evt => {
+    evt.waitUntil(precache());
+  });
+
+  self.addEventListener('fetch', evt => {
+    // bypass service worker entirely for non-cached assets so that
+    // Response.fromServiceWorker() correctly returns "false" for non-cached
+    // responses
+    if (!cacheables.includes(evt.request.url.replace(location.origin, ''))) {
+      return;
+    }
+    evt.respondWith(fromCache(evt.request));
+  });
+
+  async function precache() {
+    const cache = await caches.open(CACHE_NAME);
+    return cache.addAll(cacheables);
+  }
+
+  async function fromCache(request) {
+    const cache = await caches.open(CACHE_NAME);
+    const match = await cache.match(request);
+    return match || fetch(request);
+  }
+};
