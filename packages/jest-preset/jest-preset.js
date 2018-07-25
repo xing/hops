b021437ee248344c29a6dc98706992d2758e0997
Robin Drexler <robin.drexler@xing.com>
2018-07-25 11:17:07 +0200

    feat(jest-preset): add typescript support

---
+++ packages/jest-preset/jest-preset.js
@@ -0,0 +1,18 @@
+const { defaults } = require('jest-config');
+
+module.exports = {
+  moduleNameMapper: {
+    '^.+\\.(png|gif|jpe?g|webp|html|svg|((o|t)tf)|woff2?|ico)$':
+      'jest-preset-hops/mocks/file.js',
+    '^.+\\.tpl$': 'jest-preset-hops/mocks/tpl.js',
+    '^.+\\.css$': 'identity-obj-proxy',
+  },
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+  transform: {
+    '^.+\\.(js|jsx|mjs)$': 'jest-preset-hops/transforms/babel.js',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(gql|graphql)$': 'jest-preset-hops/transforms/graphql.js',
+  },
+  transformIgnorePatterns: [],
+  setupFiles: ['regenerator-runtime/runtime'],
+};
