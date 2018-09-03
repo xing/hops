39bc5292da9bb0913097a4c85a70ab003e8e412d
Robin Drexler <robin.drexler@xing.com>
2018-09-03 11:39:23 +0200

    fix(react): upgrade babel plugins to v7

--- packages/react/mixin.core.js
+++ packages/react/mixin.core.js
@@ -3,8 +3,8 @@ const { Mixin } = require('hops-mixin');
 class ReactCoreMixin extends Mixin {
   configureBuild(webpackConfig, { jsLoaderConfig }) {
     jsLoaderConfig.options.plugins.push(
-      require.resolve('babel-plugin-transform-class-properties'),
-      require.resolve('babel-plugin-transform-object-rest-spread')
+      require.resolve('@babel/plugin-proposal-class-properties'),
+      require.resolve('@babel/plugin-proposal-object-rest-spread')
     );
 
     return webpackConfig;
