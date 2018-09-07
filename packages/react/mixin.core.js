9cf7791ae7d358daa4add47236c7409a14ceb361
Robin Drexler <robin.drexler@xing.com>
2018-09-07 16:43:53 +0200

    fix(react): Ensure flow babel plugin runs before class properties plugin
    there is an issue with flow and class properties where values are undefined when they should not
    
    https://github.com/babel/babel/issues/8417

--- packages/react/mixin.core.js
+++ packages/react/mixin.core.js
@@ -3,6 +3,7 @@ const { Mixin } = require('hops-mixin');
 class ReactCoreMixin extends Mixin {
   configureBuild(webpackConfig, { jsLoaderConfig }) {
     jsLoaderConfig.options.plugins.push(
+      require.resolve('@babel/plugin-transform-flow-strip-types'),
       require.resolve('@babel/plugin-proposal-class-properties'),
       require.resolve('@babel/plugin-proposal-object-rest-spread')
     );
