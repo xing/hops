873bcff3fe83f5b91ada94eb060ff4e4b3ffd612
Robin Drexler <robin.drexler@xing.com>
2018-09-07 15:59:24 +0200

    fix(jest-preset-hops): Add missing flow preset

--- packages/jest-preset/transforms/babel.js
+++ packages/jest-preset/transforms/babel.js
@@ -13,6 +13,7 @@ module.exports = babelJest.createTransformer({
     '@babel/preset-react',
   ],
   plugins: [
+    require.resolve('@babel/plugin-transform-flow-strip-types'),
     require.resolve('babel-plugin-dynamic-import-node'),
     require.resolve('@babel/plugin-proposal-class-properties'),
     require.resolve('@babel/plugin-proposal-object-rest-spread'),
