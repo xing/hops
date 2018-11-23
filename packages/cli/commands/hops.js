0f5f36dc6669a89cffba0e2aeeac71b47f22a090
Robin Drexler <robin.drexler@xing.com>
2018-11-23 15:29:12 +0100

    refactor: simplify hops cli implementation
    @untool/yargs is guaranteed to be installed so we can simply require it

--- packages/cli/commands/hops.js
+++ packages/cli/commands/hops.js
@@ -2,29 +2,9 @@
 'use strict';
 
 const { dirname, join } = require('path');
+const { configure } = require('@untool/yargs');
 
-const { sync: findUp } = require('find-up');
-const { sync: resolve } = require('enhanced-resolve');
-
-const getRootDir = () => {
-  const pkgFile = findUp('package.json');
-  if (!pkgFile) throw new Error("Can't resolve package.json");
-  return dirname(pkgFile);
-};
-
-try {
-  const rootDir = getRootDir();
-  const yargsPath = resolve(rootDir, '@untool/yargs');
-  const { configure: configureYargs } = require(yargsPath);
-  configureYargs({
-    untoolNamespace: 'hops',
-    mixins: [join(__dirname, '..')],
-  }).run();
-} catch (error) {
-  if (error.message && error.message.startsWith("Can't resolve")) {
-    console.error("Error: Can't find @untool/yargs \n");
-  } else {
-    console.error(error.stack ? error.stack.toString() : error.toString());
-  }
-  process.exit(1);
-}
+configure({
+  untoolNamespace: 'hops',
+  mixins: [join(__dirname, '..')],
+}).run();
