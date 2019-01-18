99093a99c9870cd479c47c7ed46ef69d2c1b343f
Emanuel Kluge <emanuel.kluge@xing.com>
2019-01-18 10:31:52 +0100

    refactor(typescript): integrate ts-loader in a non-breaking way

--- packages/typescript/mixin.core.js
+++ packages/typescript/mixin.core.js
@@ -1,20 +1,22 @@
 const { Mixin } = require('hops-mixin');
 
 class TypescriptMixin extends Mixin {
-  configureBuild(webpackConfig, { jsLoaderConfig }) {
-    jsLoaderConfig.test = [/\.tsx?$/];
-    jsLoaderConfig.use = [
-      {
-        loader: jsLoaderConfig.loader,
-        options: { ...jsLoaderConfig.options },
-      },
-      {
-        loader: require.resolve('ts-loader'),
-      },
-    ];
-    delete jsLoaderConfig.loader;
-    delete jsLoaderConfig.options;
+  configureBuild(webpackConfig, { jsLoaderConfig, allLoaderConfigs }) {
+    const { loader, options, exclude } = jsLoaderConfig;
 
+    allLoaderConfigs.unshift({
+      test: /\.tsx?$/,
+      exclude,
+      use: [
+        {
+          loader,
+          options,
+        },
+        {
+          loader: require.resolve('ts-loader'),
+        },
+      ],
+    });
     webpackConfig.resolve.extensions.push('.ts', '.tsx');
   }
 }
