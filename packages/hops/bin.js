3094350830c8c6a90a98d0112197474293ab9998
Robin Drexler <drexler.robin@gmail.com>
2019-03-01 23:14:26 +0100

    feat(hops): add cli flag to analyze client bundle

--- packages/hops/bin.js
+++ packages/hops/bin.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 'use strict';
-
+const path = require('path');
 const { configure } = require('@untool/yargs');
 
 configure({
   untoolNamespace: 'hops',
-  mixins: [__dirname],
+  mixins: [__dirname, path.join(__dirname, 'mixins', 'analyzer')],
 }).run();
