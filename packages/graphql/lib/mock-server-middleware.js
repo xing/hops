0f7c501f211cb5632e3515f61c87e19073fda65a
Jonas Holland <mail@jhio.de>
2019-03-04 13:56:37 +0100

    fix(graphql): only initialize Apollo server once
    instead per request as untool will reload this file anyway.

--- packages/graphql/lib/mock-server-middleware.js
+++ packages/graphql/lib/mock-server-middleware.js
@@ -4,31 +4,31 @@ import express from 'express';
 import hopsConfig from 'hops-config';
 import schema from 'hops-graphql/schema';
 
-const app = express();
-app.use(cookieParser());
-
-const schemaPromise = Promise.resolve(
+const apolloAppPromise = Promise.resolve(
   typeof schema === 'function' ? schema() : schema
-);
+).then(resolvedSchema => {
+  const app = express();
+  app.use(cookieParser());
+
+  const server = new ApolloServer({
+    schema: resolvedSchema,
+    context: context => ({ ...context, config: hopsConfig }),
+  });
+
+  server.applyMiddleware({
+    app,
+    path: hopsConfig.graphqlMockServerPath,
+    cors: {
+      credentials: true,
+      origin: true,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      allowedHeaders: 'content-type',
+    },
+  });
+
+  return app;
+});
 
 export default (req, res, next) => {
-  schemaPromise.then(schema => {
-    const server = new ApolloServer({
-      schema,
-      context: ({ req }) => ({ req, config: hopsConfig }),
-    });
-
-    server.applyMiddleware({
-      app,
-      path: hopsConfig.graphqlMockServerPath,
-      cors: {
-        credentials: true,
-        origin: true,
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        allowedHeaders: 'content-type',
-      },
-    });
-
-    app.handle(req, res, next);
-  });
+  apolloAppPromise.then(app => app.handle(req, res, next), next);
 };
