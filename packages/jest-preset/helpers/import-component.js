0a816ac4aadb83349ef080690913029005c8d8b1
Emanuel Kluge <emanuel.kluge@xing.com>
2019-03-05 17:14:31 +0100

    feat(jest-preset): add babel plugin for transpiling `importComponent`

---
+++ packages/jest-preset/helpers/import-component.js
@@ -0,0 +1,11 @@
+/* eslint react/prop-types: 0 */
+const React = require('react');
+
+module.exports = ({ component }, name = 'default') => {
+  const resolve = typeof name === 'function' ? name : module => module[name];
+
+  //eslint-disable-next-line no-unused-vars
+  return function Import({ loader, render, ...ownProps }) {
+    return React.createElement(resolve(component), ownProps);
+  };
+};
