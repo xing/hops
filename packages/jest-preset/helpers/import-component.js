2d5c138ee41ec49b8cffe92e2a968b2dff85b2d8
Emanuel Kluge <emanuel.kluge@xing.com>
2019-03-07 11:39:47 +0100

    docs(jest-preset): add explaining comment to babel plugin

--- packages/jest-preset/helpers/import-component.js
+++ packages/jest-preset/helpers/import-component.js
@@ -4,7 +4,7 @@ const React = require('react');
 module.exports = ({ component }, name = 'default') => {
   const resolve = typeof name === 'function' ? name : module => module[name];
 
-  //eslint-disable-next-line no-unused-vars
+  // eslint-disable-next-line no-unused-vars
   return function Import({ loader, render, ...ownProps }) {
     return React.createElement(resolve(component), ownProps);
   };
