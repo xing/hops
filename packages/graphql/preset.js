b1bef6e1ef222865095e1180d88a755f115a3922
ZauberNerd <zaubernerd@zaubernerd.de>
2019-03-27 18:38:44 +0100

    feat: add config schema validation

--- packages/graphql/preset.js
+++ packages/graphql/preset.js
@@ -1,9 +1,30 @@
 module.exports = {
   mixins: [__dirname],
   fragmentsFile: '<rootDir>/fragmentTypes.json',
-  graphqlUri: '',
-  graphqlSchemaFile: '',
-  graphqlMockSchemaFile: '',
   graphqlMockServerPath: '/graphql',
   shouldPrefetchOnServer: true,
+  configSchema: {
+    fragmentsFile: {
+      type: 'string',
+      absolutePath: true,
+    },
+    graphqlUri: {
+      type: 'string',
+      format: 'uri',
+    },
+    graphqlSchemaFile: {
+      type: 'string',
+      absolutePath: true,
+    },
+    graphqlMockSchemaFile: {
+      type: 'string',
+      absolutePath: true,
+    },
+    graphqlMockServerPath: {
+      type: 'string',
+    },
+    shouldPrefetchOnServer: {
+      type: 'boolean',
+    },
+  },
 };
