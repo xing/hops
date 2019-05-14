8073ba800312eb20280faff03a9b15955c537986
Emanuel Kluge <emanuel.kluge@xing.com>
2019-05-14 17:04:09 +0200

    refactor(spec): use correct server address in GraphQL test

--- packages/spec/integration/graphql/mixin.runtime.js
+++ packages/spec/integration/graphql/mixin.runtime.js
@@ -2,22 +2,25 @@ const { Mixin } = require('hops-mixin');
 const fetch = require('cross-fetch');
 const { HttpLink } = require('apollo-link-http');
 
-const customFetch = getServerAddress => async (uri, options) => {
+const customFetch = serverAddress => (uri, options) => {
   const urlSuffix =
     typeof Headers !== 'undefined' && options.headers instanceof Headers
       ? options.headers.get('x-url-suffix')
       : options.headers['x-url-suffix'];
-  const newUri = urlSuffix ? `${getServerAddress()}/graphql/${urlSuffix}` : uri;
+  const newUri = urlSuffix ? `${serverAddress}/graphql/${urlSuffix}` : uri;
 
-  return await fetch(newUri, options);
+  return fetch(newUri, options);
 };
 
 module.exports = class GraphQlMixin extends Mixin {
+  bootstrap(_, res) {
+    this.root = res ? res.locals.url : '';
+  }
+
   getApolloLink() {
     return new HttpLink({
       uri: this.config.graphqlUri,
-      // TODO: get correct server address
-      fetch: customFetch(() => this.serverAddress || 'http://localhost:8080'),
+      fetch: customFetch(this.root),
     });
   }
 };
