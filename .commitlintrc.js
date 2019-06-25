f7aaee23bccd2dc4c7b8852326119caaaac3ffeb
ZauberNerd <zaubernerd@zaubernerd.de>
2019-06-25 16:13:47 +0200

    chore: use @commitlint/config-lerna-scopes to find valid scopes

---
+++ .commitlintrc.js
@@ -0,0 +1,16 @@
+const { getPackages } = require('@commitlint/config-lerna-scopes').utils;
+
+module.exports = {
+  extends: [
+    '@commitlint/config-conventional',
+    '@commitlint/config-lerna-scopes',
+  ],
+  rules: {
+    'scope-enum': context =>
+      getPackages(context).then(packages => [
+        2,
+        'always',
+        packages.map(p => p.replace(/^hops-|-hops$/, '')),
+      ]),
+  },
+};
