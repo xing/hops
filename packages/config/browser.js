bff37299634f0f757d6d3f1d16bad4192a6f7711
Emanuel Kluge <emanuel.kluge@xing.com>
2019-09-27 12:52:42 +0200

    chore(config): deprecate hops-config package
    Instead of importing the `config`-object from a package, you should
    rather use `ConfigContext` or the `withConfig()`-HOC from `hops`.
    
    **Example using `ConfigContext`**
    ```jsx
    import React from 'react';
    import { ConfigContext } from 'hops';
    
    const Comp = () => {
      const config = React.useContext(ConfigContext);
      return <em>{config.basePath}</em>;
    };
    ```
    
    **Example using `withConfig` HOC**
    ```jsx
    import React from 'react';
    import { withConfig } from 'hops';
    
    const Comp = withConfig(({ config }) => (
      <em>{config.basePath}</em>
    ));
    ```

--- packages/config/browser.js
+++ packages/config/browser.js
@@ -1,1 +1,7 @@
+const deprecate = require('depd')('hops-config');
+
+deprecate(
+  '[DEP0001] hops-config is deprecated. Please use ConfigContext or the withConfig() HOC from hops instead.'
+);
+
 module.exports = require('@untool/core').internal.getConfig();
