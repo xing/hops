b3fef5a18f9698ad68146745f13650622d0f71c1
ZauberNerd <zaubernerd@zaubernerd.de>
2019-10-22 18:14:09 +0200

    fix(jest-preset): alias core-js to correct version

--- packages/jest-preset/transforms/babel.js
+++ packages/jest-preset/transforms/babel.js
@@ -1,4 +1,5 @@
-var babelJest = require('babel-jest');
+const babelJest = require('babel-jest');
+const { dirname } = require('path');
 
 module.exports = babelJest.createTransformer({
   presets: [
@@ -14,6 +15,7 @@ module.exports = babelJest.createTransformer({
     '@babel/preset-react',
   ],
   plugins: [
+    pluginRenameCoreJS,
     require.resolve('../helpers/babel-plugin-import-component'),
     require.resolve('@babel/plugin-transform-flow-strip-types'),
     require.resolve('babel-plugin-dynamic-import-node'),
@@ -21,3 +23,34 @@ module.exports = babelJest.createTransformer({
   ],
   babelrc: false,
 });
+
+function pluginRenameCoreJS() {
+  function replaceSource(source) {
+    source.value = source.value.replace(
+      /^core-js/,
+      dirname(require.resolve('core-js/package.json'))
+    );
+  }
+
+  function isRequire(path) {
+    if (!path.get('callee').isIdentifier({ name: 'require' })) return false;
+    if (path.node.arguments.length === 0) return false;
+    if (!path.get('arguments.0').isStringLiteral()) return false;
+    return true;
+  }
+
+  const visitor = {
+    ImportDeclaration(path) {
+      replaceSource(path.node.source);
+    },
+    CallExpression(path) {
+      if (isRequire(path)) {
+        replaceSource(path.node.arguments[0]);
+      }
+    },
+  };
+
+  return {
+    visitor,
+  };
+}
