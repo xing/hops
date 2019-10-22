685aa1a7cc4dbfb2a7cfc93c07d15b67801905af
ZauberNerd <zaubernerd@zaubernerd.de>
2019-10-22 11:47:19 +0200

    feat(pwa): whitelist config keys that should be sent to the browser

--- packages/pwa/preset.js
+++ packages/pwa/preset.js
@@ -4,6 +4,10 @@ module.exports = {
   workerPath: '<basePath>/sw.js',
   workerFile: path.join(__dirname, 'worker.js'),
   mixins: [__dirname],
+  browserWhitelist: {
+    workerPath: true,
+    locations: true,
+  },
   configSchema: {
     workerPath: { type: 'string', minLength: 1 },
     workerFile: { type: 'string', absolutePath: true },
