778686419e5f4fed0beb8263ba8fdec56e99f01f
ZauberNerd <zaubernerd@zaubernerd.de>
2019-10-24 11:18:10 +0200

    fix: switch to react-helmet-async

--- packages/template-react/src/home/spec/index.spec.js
+++ packages/template-react/src/home/spec/index.spec.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { HelmetProvider } from 'react-helmet-async';
 import Home from '../';
 
+HelmetProvider.canUseDOM = false;
+
 it('renders correctly', () => {
-  const tree = renderer.create(<Home />).toJSON();
+  const tree = renderer
+    .create(
+      <HelmetProvider>
+        <Home />
+      </HelmetProvider>
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
