f35eaaf532ad80c5fa41f7f659231e1e7e6abd69
Jonas Holland <mail@jhio.de>
2019-10-25 14:59:41 +0200

    feat(apollo): introduce package

---
+++ packages/apollo/index.d.ts
@@ -0,0 +1,16 @@
+import { ApolloClientOptions } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { ApolloCache } from 'apollo-cache';
+import { Omit } from 'hops';
+
+declare module 'hops-apollo' {
+  interface ApolloClientOptionsHops<TCacheShape>
+    extends Omit<Omit<ApolloClientOptions<TCacheShape>, 'link'>, 'cache'> {
+    link?: ApolloLink;
+    cache?: ApolloCache<TCacheShape>;
+  }
+
+  export interface HopsApolloOptions<TCacheShape = any> {
+    graphql?: ApolloClientOptionsHops<TCacheShape>;
+  }
+}
