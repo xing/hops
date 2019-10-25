606eda7b5588e88936393f505cf5383eb19e96a4
Jonas Holland <mail@jhio.de>
2019-10-25 15:04:08 +0200

    refactor(graphql): use hops-apollo and hops-apollo-mock-server

--- packages/graphql/index.d.ts
+++ packages/graphql/index.d.ts
@@ -1,15 +1,6 @@
-import { ApolloClientOptions } from 'apollo-client';
-import { ApolloLink } from 'apollo-link';
-import { ApolloCache } from 'apollo-cache';
-import { Omit } from 'hops';
+import { ApolloClientOptionsHops } from 'hops-apollo';
 
 declare module 'hops-graphql' {
-  interface ApolloClientOptionsHops<TCacheShape>
-    extends Omit<Omit<ApolloClientOptions<TCacheShape>, 'link'>, 'cache'> {
-    link?: ApolloLink;
-    cache?: ApolloCache<TCacheShape>;
-  }
-
   export interface HopsGraphqlOptions<TCacheShape = any> {
     graphql?: ApolloClientOptionsHops<TCacheShape>;
   }
