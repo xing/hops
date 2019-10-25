e72a2734e7be6e6d5782ed14b230b5f1f91e0025
Jonas Holland <mail@jhio.de>
2019-10-25 14:59:41 +0200

    feat(apollo): introduce package

---
+++ packages/apollo/lib/fragments.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const fetch = require('cross-fetch');
+const { access, readFile, writeFile } = require('fs');
+const { promisify } = require('util');
+const { graphql } = require('graphql');
+const { makeExecutableSchema } = require('graphql-tools');
+
+function writeFragmentTypesFile(fragmentsFile, result) {
+  result.data.__schema.types = result.data.__schema.types.filter(
+    t => t.possibleTypes !== null
+  );
+  return promisify(writeFile)(fragmentsFile, JSON.stringify(result.data));
+}
+
+function executeRemoteQuery(graphqlUri, optionalHeaders = [], query) {
+  const headers = optionalHeaders.reduce(
+    (headers, header) => {
+      const parts = header.split(':');
+      headers[parts.shift()] = parts.join(':');
+      return headers;
+    },
+    {
+      'Content-Type': 'application/json',
+    }
+  );
+  return fetch(graphqlUri, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({ query }),
+  }).then(result => result.json());
+}
+
+function executeLocalQuery(schemaFile, query) {
+  return promisify(readFile)(schemaFile, 'utf-8')
+    .then(typeDefs =>
+      makeExecutableSchema({
+        typeDefs,
+        resolverValidationOptions: {
+          requireResolversForResolveType: false,
+        },
+      })
+    )
+    .then(executableSchema => graphql(executableSchema, query));
+}
+
+const query = `
+query IntrospectionQuery {
+  __schema {
+    types {
+      kind
+      name
+      possibleTypes {
+        name
+      }
+    }
+  }
+}
+`;
+
+module.exports = function generateFragmentTypes(options) {
+  return promisify(access)(options.schemaFile)
+    .then(
+      () => executeLocalQuery(options.schemaFile, query),
+      () => executeRemoteQuery(options.graphqlUri, options.headers, query)
+    )
+    .then(result => writeFragmentTypesFile(options.fragmentsFile, result));
+};
