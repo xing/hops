1258fba6a337ec69a65341a5d99e1896c6195e3b
Emanuel Kluge <emanuel.kluge@xing.com>
2019-12-13 15:19:02 +0100

    feat(react): warn on non-whitelisted config property
    ...when it's retrieved in the browser runtime.

--- packages/react/config/mixin.runtime.js
+++ packages/react/config/mixin.runtime.js
@@ -5,9 +5,33 @@ const { Mixin } = require('hops-mixin');
 
 const { Provider } = require('./context');
 
+const warnOnIncompleteBrowserWhitelist = config => {
+  if (process.env.NODE_ENV === 'development' && typeof Proxy === 'function') {
+    return new Proxy(config, {
+      get: (obj, prop) => {
+        const value = obj[prop];
+
+        if (!Object.prototype.hasOwnProperty.call(obj, prop)) {
+          console.warn(
+            `The property "${prop}" does not exist. Did you forget to add it to the browserWhitelist of your Hops config?`
+          );
+        }
+
+        return value;
+      },
+    });
+  }
+
+  return config;
+};
+
 class HopsReactConfigMixin extends Mixin {
   enhanceElement(reactElement) {
-    return React.createElement(Provider, { value: this.config }, reactElement);
+    return React.createElement(
+      Provider,
+      { value: warnOnIncompleteBrowserWhitelist(this.config) },
+      reactElement
+    );
   }
 }
 
