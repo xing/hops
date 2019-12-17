ca3263f441f40f691773ab0cac10b2e5d12b2d7e
Emanuel Kluge <emanuel.kluge@xing.com>
2019-12-17 09:52:42 +0100

    feat(lambda): make node version configurable
    BREAKING CHANGE: remove support for `nodejs8.10`.

--- packages/lambda/lib/aws-config.js
+++ packages/lambda/lib/aws-config.js
@@ -1,9 +1,25 @@
 'use strict';
 
 const { trimSlashes } = require('pathifist');
+const semver = require('semver');
+
+const getRuntimeNodeVersion = nodeVersion => {
+  const { major } = semver.coerce(nodeVersion);
+
+  switch (major) {
+    case 10:
+      return 'nodejs10.x';
+    case 12:
+      return 'nodejs12.x';
+    default:
+      throw new Error(`Node version ${nodeVersion} is not supported.`);
+  }
+};
 
 module.exports = function getAWSConfig(hopsConfig) {
   const awsConfig = hopsConfig._aws || hopsConfig.aws || {};
+  const nodeVersion =
+    hopsConfig.node === 'current' ? process.version : hopsConfig.node;
 
   const region =
     awsConfig.region ||
@@ -14,6 +30,7 @@ module.exports = function getAWSConfig(hopsConfig) {
     region,
     stackName: awsConfig.uniqueName,
     bucketName: awsConfig.uniqueName,
+    runtime: getRuntimeNodeVersion(nodeVersion),
     memorySize: awsConfig.memorySize,
     stageName: awsConfig.stageName,
     domainName: awsConfig.domainName || '',
