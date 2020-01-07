4d29ef8b98a2d8e3970e6afe378477c8227a19bd
Markus Wolf <markus.wolf@new-work.se>
2020-01-07 15:25:23 +0100

    test: add test case for imports
    Imports with css-modules (postcss-loader and css-loader) where missing
    sufficent test cases for possible imports.
    
    This test adds different cases:
    * direct imports from css (global and local modules)
    * node_modules and local modules
    * css modules composes (relative and from node_modules)

--- packages/spec/integration/postcss/__tests__/build-static.js
+++ packages/spec/integration/postcss/__tests__/build-static.js
@@ -10,18 +10,34 @@ describe('postcss production static build', () => {
     const { page } = await createPage();
     await page.goto(url, { waitUntil: 'networkidle2' });
 
-    const { position, fontFamily } = await page.evaluate(() => {
-      const { position, fontFamily } = window.getComputedStyle(
-        document.querySelector('h1')
-      );
-      return { position, fontFamily };
+    const {
+      position,
+      fontFamily,
+      textAlign,
+      paddingTop,
+      paddingLeft,
+    } = await page.evaluate(() => {
+      const {
+        position,
+        fontFamily,
+        textAlign,
+        paddingTop,
+        paddingLeft,
+      } = window.getComputedStyle(document.querySelector('h1'));
+      return { position, fontFamily, textAlign, paddingTop, paddingLeft };
     });
 
     expect(position).toBe('sticky');
     expect(fontFamily).toContain('-apple-system');
+    expect(textAlign).toBe('center');
+    expect(paddingTop).toBe('50px');
+    expect(paddingLeft).toBe('27.4285px');
+    // todo: add expectation for c-arrow class
 
-    const stylesheet = await page.$('link[rel=stylesheet]');
-    expect(stylesheet).not.toBeNull();
+    // there should be 2 stylesheets
+    // the global animate.css and the css-modules import
+    const stylesheets = await page.$$('link[rel=stylesheet]');
+    expect(stylesheets).toHaveLength(2);
 
     await page.close();
   });
