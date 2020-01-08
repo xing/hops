3d43f2e85ce85fc252233cc08e51f4d22e45214f
Markus Wolf <markus.wolf@new-work.se>
2020-01-08 13:58:49 +0100

    test: add favicon to all test cases
    This adds a favicon to all cases, since if run in debug mode
    (with open browser window) a request for the favicon
    is made. Since there is no favicon in the test, the request is
    replied with a 404 warning.
    
    In the spec test set, there is a check that there are no unknown
    console output in the test cases.
    Unfortunately the 404 favicon request does not report a
    path for the favicon but just a generic 404 error. Therefore
    we are not able to whitelist this message without risking
    false positives in the test.
    
    Adding an inline favicon supresses these requests entierly
    getting around the above problem.

--- packages/spec/integration/postcss/index.js
+++ packages/spec/integration/postcss/index.js
@@ -1,5 +1,6 @@
 import { render } from 'hops';
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 import styles from './styles.css';
 /* eslint-disable import/no-unresolved */
 import './global.css?global';
@@ -7,5 +8,10 @@ import 'animate.css/animate.min.css?global';
 /* eslint-enable import/no-unresolved */
 
 export default render(
-  <h1 className={`${styles.headline} fancy-headline animated`}>hello</h1>
+  <>
+    <Helmet>
+      <link rel="icon" href="data:;base64,iVBORw0KGgo=" />
+    </Helmet>
+    <h1 className={`${styles.headline} fancy-headline animated`}>hello</h1>
+  </>
 );
