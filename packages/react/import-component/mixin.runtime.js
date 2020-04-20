6b51f51431a76f2300d7d0cafcfd6f40584c57c7
ZauberNerd <zaubernerd@zaubernerd.de>
2020-04-20 18:54:44 +0200

    refactor: restructure hops-react internals
    Split mixins into separate logical groups and colocate their React
    components.

---
+++ packages/react/import-component/mixin.runtime.js
@@ -0,0 +1,21 @@
+const { Mixin } = require('hops-bootstrap');
+const { createElement } = require('react');
+const ImportComponentContext = require('./context');
+
+class ImportComponentMixin extends Mixin {
+  bootstrap(_req, res) {
+    if (res) {
+      res.locals.modules = this.modules = [];
+    }
+  }
+
+  enhanceElement(element) {
+    return createElement(
+      ImportComponentContext.Provider,
+      { value: this.modules },
+      element
+    );
+  }
+}
+
+module.exports = ImportComponentMixin;
---
+++ packages/react/router/mixin.server.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line node/no-deprecated-api
+const { parse } = require('url');
+const { Mixin } = require('hops-bootstrap');
+const { createElement } = require('react');
+const { StaticRouter } = require('react-router-dom');
+const { ensureLeadingSlash, trimTrailingSlash } = require('pathifist');
+
+class ReactRouterMixin extends Mixin {
+  constructor(config, _element, options) {
+    super(config, options);
+    this.router = {};
+  }
+
+  bootstrap(req, res) {
+    this.url = parse(req.url);
+    res.locals.router = this.router;
+  }
+
+  enhanceElement(element) {
+    const { pathname, search } = this.url;
+    const props = {
+      ...this.options.router,
+      location: { pathname, search },
+      basename: trimTrailingSlash(ensureLeadingSlash(this.config.basePath)),
+      context: this.router,
+    };
+
+    return createElement(StaticRouter, props, element);
+  }
+}
+
+module.exports = ReactRouterMixin;
