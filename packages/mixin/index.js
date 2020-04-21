fae644325a82764b09a530558a6050291190ea20
ZauberNerd <zaubernerd@zaubernerd.de>
2020-04-21 16:19:04 +0200

    chore: deprecate "hops-mixin" in favor of "hops-bootstrap"

--- packages/mixin/index.js
+++ packages/mixin/index.js
@@ -1,5 +1,10 @@
 const { Mixin } = require('hops-bootstrap');
 const { sync, async } = require('mixinable');
+const deprecate = require('depd')('hops-config');
+
+deprecate(
+  '[DEP0005] hops-mixin is deprecated. Please import "Mixin" from "hops-bootstrap" and strategies from "mixinable" https://github.com/xing/hops/blob/master/DEPRECATIONS.md#dep0005.'
+);
 
 module.exports = {
   Mixin,
