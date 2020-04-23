9841fbcc2088cabda8a91fc619ac10f93d54ee3e
ZauberNerd <zaubernerd@zaubernerd.de>
2020-04-23 12:06:15 +0200

    feat: add --fast-dev option for faster development builds

--- DOCUMENTATION.md
+++ DOCUMENTATION.md
@@ -238,6 +238,7 @@ This command will either start the development server or execute a production bu
 _The production flag (`--production` or `-p`) is a shortcut for setting the environment variable `NODE_ENV=production`, so these two are interchangeable._
 
 - `npm start` starts the development server with hot module reloading, etc.
+- `npm start -- --fast-dev` (**experimental**) starts the development server with some optimizations for faster builds (_warning:_ this will lead to different bundles than in production, use with caution).
 - `npm start -- --production` executes a production build and then starts the production server.
 - `npm start -- --production --static`¹ exports static HTML pages for all configured [locations](#default-settings) and then starts the production server (_this only works in tandem with production mode_).
 
---
+++ packages/webpack/mixins/fast-dev/mixin.core.js
@@ -0,0 +1,62 @@
+const { Mixin } = require('hops-bootstrap');
+
+function findResolved(a, b) {
+  try {
+    return a === require.resolve(b);
+  } catch {
+    return false;
+  }
+}
+
+class FastDevWebpackMixin extends Mixin {
+  configureBuild(webpackConfig, { allLoaderConfigs, jsLoaderConfig }, target) {
+    if (
+      target === 'build' ||
+      (target === 'node' && process.env.NODE_ENV === 'production')
+    ) {
+      return;
+    }
+
+    if (this.options.fastDev) {
+      process.env.__HOPS_FAST_DEV__ = 'true';
+
+      jsLoaderConfig.exclude = [/node_modules\//];
+
+      const presetEnv = jsLoaderConfig.options.presets.find((preset) => {
+        return findResolved(preset[0], '@babel/preset-env');
+      });
+
+      if (presetEnv) {
+        presetEnv[1].useBuiltIns = false;
+        delete presetEnv[1].corejs;
+      }
+
+      allLoaderConfigs
+        .filter((loader) => {
+          return loader.test instanceof RegExp && loader.test.test('.tsx');
+        })
+        .forEach((loader) => {
+          const babelLoader = loader.use.find((l) => {
+            return findResolved(l.loader, 'babel-loader');
+          });
+
+          if (babelLoader) {
+            babelLoader.options = {
+              ...babelLoader.options,
+              presets: [],
+              plugins: [
+                // eslint-disable-next-line node/no-extraneous-require
+                require.resolve('hops-react/lib/babel'),
+              ],
+            };
+          }
+        });
+    }
+  }
+
+  handleArguments(argv) {
+    this.options = { ...this.options, ...argv };
+  }
+}
+
+module.exports = FastDevWebpackMixin;
