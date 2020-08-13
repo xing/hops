7369831ed0643c9ba447ca9a5e88d2fd78f84fe2
Emanuel Kluge <emanuel.kluge@new-work.se>
2020-08-13 09:42:24 +0200

    test(spec): ensure there are no GraphQL requests on the server
    ...when SSR is turned off.

---
+++ packages/spec/integration/graphql-no-ssr/index.js
@@ -0,0 +1,34 @@
+import gql from 'graphql-tag';
+import { render } from 'hops';
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import { Query } from 'react-apollo';
+
+const query = gql`
+  query testQuery {
+    subject {
+      title
+    }
+  }
+`;
+
+export default render(
+  <>
+    <Helmet>
+      <link rel="icon" href="data:;base64,iVBORw0KGgo=" />
+    </Helmet>
+    <Query query={query}>
+      {({ loading, error, data: { subject } = {} }) => {
+        if (loading) return <strong id="loading">loading...</strong>;
+        if (error)
+          return (
+            <strong id="error">
+              Error: {error.message}
+              <pre>{JSON.stringify(error, null, 2)}</pre>
+            </strong>
+          );
+        return <p>{subject.title}</p>;
+      }}
+    </Query>
+  </>
+);
