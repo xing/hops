038a4304e69458d1dc47efe62f0fe5b73eea3c9d
Emanuel Kluge <emanuel.kluge@new-work.se>
2020-08-13 09:42:24 +0200

    test(spec): ensure there are no GraphQL requests on the server
    ...when SSR is turned off.

---
+++ packages/spec/integration/graphql-no-ssr/__tests__/develop.js
@@ -0,0 +1,20 @@
+describe('graphql mock server without SSR', () => {
+  let url;
+
+  beforeAll(async () => {
+    url = await HopsCLI.start('--fast-dev');
+  });
+
+  it('requests data on the client & not on the server', async () => {
+    const { page, getElementByText } = await createPage();
+    const response = await page.goto(url, { waitUntil: 'networkidle2' });
+    const html = await response.text();
+    const loading = '<strong id="loading">loading...</strong>';
+
+    expect(html.includes(loading)).toBe(true);
+    await page.waitForSelector('p');
+    expect(await getElementByText('Fetch No. 1')).toBeDefined();
+
+    await page.close();
+  });
+});
