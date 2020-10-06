3f4b94478d135a9e9e4279dea83f4f3384d1ed20
Emanuel Kluge <emanuel.kluge@new-work.se>
2020-10-06 16:40:28 +0200

    test(spec): check that styled-component's css prop works with TS

---
+++ packages/spec/integration/typescript-styled-components/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { render } from 'hops';
+
+export default render(
+  <h1
+    css={`
+      color: rgb(255, 0, 255);
+    `}
+  >
+    Hello World.
+  </h1>
+);
