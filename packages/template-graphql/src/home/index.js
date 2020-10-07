5b92cf1de345e07bfd818a2d49bf251a1286551f
ZauberNerd <zaubernerd@zaubernerd.de>
2020-10-07 18:15:05 +0200

    refactor: switch to XING GraphQL API for demo template

--- packages/template-graphql/src/home/index.js
+++ packages/template-graphql/src/home/index.js
@@ -1,31 +1,33 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
 import { Helmet } from 'react-helmet-async';
-import query from './commits.gql';
+import query from './jobs.gql';
 import styles from './styles.css';
 
-const withCommits = graphql(query);
+const withJobs = graphql(query);
 
-export const Home = ({ data: { loading, github: { repo } = {} } }) => (
-  <div>
-    <Helmet>
-      <title>Hops Demo</title>
-    </Helmet>
-    <h1 className={styles.headline}>Hello World!</h1>
-    {loading ? (
-      <div>loading commits...</div>
-    ) : (
-      repo.commits.map((commit) => {
-        const repoUrl = `https://github.com/${repo.owner.login}/${repo.name}`;
-        return (
-          <p key={commit.sha}>
-            <a href={`${repoUrl}/commit/${commit.sha}`}>"{commit.message}"</a>{' '}
-            by <b>{commit.author.login || commit.author.name}</b>
-          </p>
-        );
-      })
-    )}
-  </div>
-);
+export const Home = ({ data: { loading, error, jobSearchByQuery } }) => {
+  return (
+    <div>
+      <Helmet>
+        <title>Hops Demo</title>
+      </Helmet>
+      <h1 className={styles.headline}>Hello World!</h1>
+      {loading ? (
+        <div>loading...</div>
+      ) : (
+        jobSearchByQuery.collection.map(({ jobDetail }) => {
+          const company = jobDetail.companyInfo.company;
+          return (
+            <p key={jobDetail.id}>
+              <a href={jobDetail.url}>"{jobDetail.title}"</a> at{' '}
+              <b>{company ? company.companyName : 'N/A'}</b>
+            </p>
+          );
+        })
+      )}
+    </div>
+  );
+};
 
-export default withCommits(Home);
+export default withJobs(Home);
