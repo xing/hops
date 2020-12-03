7bc40e9c4b3bd9a0598b83fb0a2fa8053d501e36
Emanuel Kluge <emanuel.kluge@new-work.se>
2020-12-03 14:19:37 +0100

    chore(spec): make TS/styled-components example build
    ...instead of failing with error message:
    
    ```
    [tsl] ERROR in ./packages/spec/integration/typescript-styled-components/index.tsx(13,7)
          TS2322: Type '{ children: string; css: string; }' is not assignable to type 'DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>'.
      Property 'css' does not exist on type 'DetailedHTMLProps<HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>'.
    ```

--- packages/spec/integration/typescript-styled-components/index.tsx
+++ packages/spec/integration/typescript-styled-components/index.tsx
@@ -1,6 +1,14 @@
 import * as React from 'react';
 import { render } from 'hops';
 
+// Fixes the TS compilation for the CSS prop.
+// Importing the empty object is actually required,
+// otherwise the error "Module not found: Error:
+// Can't resolve 'styled-components/cssprop'"
+// is thrown.
+// https://styled-components.com/docs/api#usage-with-typescript
+import {} from 'styled-components/cssprop';
+
 export default render(
   <h1
     css={`
