5310fd5433ed200b4759065f43bee09f9147d4d0
Emanuel Kluge <emanuel.kluge@new-work.se>
2021-01-06 16:32:57 +0100

    refactor(spec): improve API of HopsCLI.start

--- packages/spec/helpers/test-helpers.js
+++ packages/spec/helpers/test-helpers.js
@@ -2,6 +2,7 @@ const { spawn } = require('child_process');
 const path = require('path');
 const { promisify } = require('util');
 const rimraf = promisify(require('rimraf'));
+const EProm = require('eprom');
 
 const { copy } = require('fs-extra');
 const mktemp = require('mktemp').createDir;
@@ -32,52 +33,57 @@ const build = ({ cwd, env = {}, argv = [] }) =>
     process.on('exit', () => buildProcess.kill());
   });
 
-const startServer = ({ cwd, command, env = {}, argv = [] }) =>
-  new Promise((resolve, reject) => {
-    let onTeardown;
-    const teardownPromise = new Promise((resolve) => (onTeardown = resolve));
+const startServer = ({ cwd, command, env = {}, argv = [] }) => {
+  const teardownPromise = new EProm();
+  const urlPromise = new EProm();
+  let stdout = '';
+  let stderr = '';
 
-    const hopsBin = resolveFrom(cwd, 'hops/bin');
-
-    const args = [hopsBin, command].concat(argv);
-    debug('Spawning server', [hopsBin, ...args].join(' '));
-    const started = spawn(process.argv[0], args, {
-      env,
-      cwd,
-    });
-    const teardown = () => {
-      started.kill();
-      return teardownPromise;
-    };
-
-    started.stdout.on('data', (data) => {
-      const line = data.toString('utf-8');
-      debug('stdout >', line);
-      const match = line.match(/listening at (.*)/i);
-      if (match) {
-        debug('found match:', match[1]);
-        const url = match[1];
-        resolve({ url, teardown });
-      }
-    });
-
-    let stderr = '';
-    started.stderr.on('data', (data) => {
-      const line = data.toString('utf-8');
-      debug('stderr >', line);
-      stderr += line;
-    });
-
-    started.on('close', (code) => {
-      debug('Server stopped. exitcode:', code);
-      onTeardown(stderr);
-      if (code) {
-        reject(stderr);
-      }
-    });
-
-    started.on('error', (error) => reject(error));
+  const hopsBin = resolveFrom(cwd, 'hops/bin');
+  const args = [hopsBin, command].concat(argv);
+  debug('Spawning server', args.join(' '), { env });
+  const started = spawn(process.argv[0], args, {
+    env,
+    cwd,
   });
+  let stopServer = () => {
+    started.kill();
+    teardownPromise.resolve({ stderr, stdout });
+    return teardownPromise;
+  };
+
+  started.stdout.on('data', (data) => {
+    const line = data.toString('utf-8');
+    debug('stdout >', line);
+    stdout += line;
+
+    const [, url] = line.match(/listening at (.*)/i) || [];
+    if (url) {
+      debug('found match:', url);
+      urlPromise.resolve(url);
+    }
+  });
+
+  started.stderr.on('data', (data) => {
+    const line = data.toString('utf-8');
+    debug('stderr >', line);
+    stderr += line;
+  });
+
+  started.on('close', (code) => {
+    debug('Server stopped. exitcode:', code);
+    urlPromise.reject('server closed');
+    stopServer = () => {};
+  });
+
+  started.on('error', (error) => teardownPromise.reject(error));
+
+  process.on('exit', () => {
+    stopServer();
+  });
+
+  return { getUrl: () => urlPromise, stopServer };
+};
 
 const isPuppeteerDisabled = (pragmas) => {
   if (pragmas['jest-hops-puppeteer'] === 'off') {
