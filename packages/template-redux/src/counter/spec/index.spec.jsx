56ad96e417d53e6c980a0e878c2e2489fb8708ba
Jonas Holland <mail@jhio.de>
2021-03-04 18:05:27 +0100

    chore: rename files which use JSX syntax to have the .jsx extension

--- packages/template-redux/src/counter/spec/index.spec.jsx
+++ packages/template-redux/src/counter/spec/index.spec.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import CounterContainer, { mapStateToProps } from '../';
