66ce409530cdfe07015798348b4ae157ccf91b52
Emanuel Kluge <emanuel.kluge@new-work.se>
2021-03-17 08:14:04 +0100

    test(postcss): check namespace & default imports of CSS Modules

--- packages/spec/integration/postcss/index.js
+++ packages/spec/integration/postcss/index.js
@@ -1,7 +1,13 @@
 import { render } from 'hops';
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+
+// Verify both namespace & default imports work fine
+// while we're still using module type CommonJS
+// for CSS Modules.
 import * as styles from './styles.css';
+import otherStyles from './other-styles.css';
+
 /* eslint-disable import/no-unresolved */
 import './global.css?global';
 import 'animate.css/animate.min.css?global';
@@ -12,7 +18,9 @@ export default render(
     <Helmet>
       <link rel="icon" href="data:;base64,iVBORw0KGgo=" />
     </Helmet>
-    <h1 className={`${styles.headline} fancy-headline animate__animated`}>
+    <h1
+      className={`${styles.headline} ${otherStyles.strike} fancy-headline animate__animated`}
+    >
       hello
     </h1>
   </>
