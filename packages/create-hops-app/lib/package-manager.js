4363e09da43b6d9ea20969f633a0c192be4804a1
Emanuel Kluge <emanuel.kluge@new-work.se>
2021-06-24 07:54:24 +0200

    fix(create-hops-app): remove intermediary hack
    …for introducing support for Node 16

--- packages/create-hops-app/lib/package-manager.js
+++ packages/create-hops-app/lib/package-manager.js
@@ -37,10 +37,6 @@ function installPackages(packages, type, options) {
     if (type === 'dev') {
       command.push('--dev');
     }
-    // TODO: remove this after PR #1878 has been released
-    if (process.version.startsWith('v16')) {
-      command.push('--ignore-engines');
-    }
   } else {
     command =
       packages.length === 0
