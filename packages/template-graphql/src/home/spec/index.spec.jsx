1be6c9ed713c67be3d45879f703328e07a264aec
ZauberNerd <zaubernerd@zaubernerd.de>
2021-06-24 12:38:49 +0000

    test(template-graphql): add test with msw
    Co-authored-by: Markus Wolf <markus.wolf@new-work.se>
    Co-authored-by: Emanuel Kluge <emanuel.kluge@new-work.se>

--- packages/template-graphql/src/home/spec/index.spec.jsx
+++ packages/template-graphql/src/home/spec/index.spec.jsx
@@ -1,7 +1,9 @@
+import { mockServer, graphql } from 'hops-msw/unit';
+import { withApolloTestProvider } from 'hops-react-apollo';
 import React from 'react';
-import renderer from 'react-test-renderer';
 import { HelmetProvider } from 'react-helmet-async';
-import { Home } from '../';
+import renderer, { act } from 'react-test-renderer';
+import HomeWithData, { Home } from '../index.jsx';
 
 HelmetProvider.canUseDOM = false;
 
@@ -40,5 +42,52 @@ it('renders loaded state correctly', () => {
       <Home loading={false} data={data} />
     </HelmetProvider>
   );
+  expect(tree).toMatchSnapshot();
+});
+
+it('loads graphql data', async () => {
+  mockServer.use(
+    graphql.query('search', (req, res, ctx) => {
+      return res(
+        ctx.data({
+          __typename: 'Query',
+          jobSearchByQuery: {
+            __typename: 'JobSearchResult',
+            collection: [
+              {
+                __typename: 'JobItemResult',
+                jobDetail: {
+                  __typename: 'VisibleJob',
+                  id: '1',
+                  url: 'https://www.xing.com/jobs/1',
+                  title: 'some job',
+                  companyInfo: {
+                    __typename: 'JobCompanyInfo',
+                    company: {
+                      __typename: 'Company',
+                      companyName: 'cool company',
+                    },
+                  },
+                },
+              },
+            ],
+          },
+        })
+      );
+    })
+  );
+
+  const tree = renderer.create(
+    withApolloTestProvider(
+      <HelmetProvider>
+        <HomeWithData />
+      </HelmetProvider>
+    )
+  );
+
+  await act(async () => {
+    await new Promise((res) => setTimeout(res, 10));
+  });
+
   expect(tree).toMatchSnapshot();
 });
