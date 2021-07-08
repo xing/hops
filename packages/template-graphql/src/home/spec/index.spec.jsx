f84f4a38eb2140eeb958ab22ea5eb469ba64f692
ZauberNerd <zaubernerd@zaubernerd.de>
2021-07-08 13:35:48 +0000

    feat(template-graphql): use msw for development mode

--- packages/template-graphql/src/home/spec/index.spec.jsx
+++ packages/template-graphql/src/home/spec/index.spec.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import renderer, { act } from 'react-test-renderer';
 import HomeWithData, { Home } from '../index.jsx';
+import { jobSearchByQueryData } from '../../../mocks';
 
 HelmetProvider.canUseDOM = false;
 
@@ -49,32 +50,7 @@ it('renders loaded state correctly', () => {
 it('loads graphql data', async () => {
   getMockServer().use(
     graphql.query('search', (req, res, ctx) => {
-      return res(
-        ctx.data({
-          __typename: 'Query',
-          jobSearchByQuery: {
-            __typename: 'JobSearchResult',
-            collection: [
-              {
-                __typename: 'JobItemResult',
-                jobDetail: {
-                  __typename: 'VisibleJob',
-                  id: '1',
-                  url: 'https://www.xing.com/jobs/1',
-                  title: 'some job',
-                  companyInfo: {
-                    __typename: 'JobCompanyInfo',
-                    company: {
-                      __typename: 'Company',
-                      companyName: 'cool company',
-                    },
-                  },
-                },
-              },
-            ],
-          },
-        })
-      );
+      return res(ctx.data(jobSearchByQueryData()));
     })
   );
 
