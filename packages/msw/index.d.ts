37686d8f89af38225c77f6a4e79a4fcb5cd52da8
ZauberNerd <zaubernerd@zaubernerd.de>
2021-07-26 10:57:55 +0000

    fix(msw): correct type definition for main export

--- packages/msw/index.d.ts
+++ packages/msw/index.d.ts
@@ -1,6 +1,6 @@
 import { graphql as mswGraphQL, rest as mswRest } from 'msw';
 
-declare module 'hops-msw/unit' {
+declare module 'hops-msw' {
   export const rest: typeof mswRest;
   export const graphql: typeof mswGraphQL;
 }
