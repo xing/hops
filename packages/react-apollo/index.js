01ffd508675d39d78950a9efb64e88e370530dd4
Robert Kowalski <robert.kowalski@new-work.se>
2021-09-03 14:09:33 +0200

    fix: remove deprecated apollo 2
    BREAKING CHANGE: removes deprecated apollo 2 client

--- packages/react-apollo/index.js
+++ packages/react-apollo/index.js
@@ -4,25 +4,10 @@ const { fetch } = require('cross-fetch');
 let client;
 
 const { ApolloClient, HttpLink, ApolloProvider, InMemoryCache } = (() => {
-  try {
-    const {
-      ApolloClient,
-      HttpLink,
-      ApolloProvider,
-    } = require('@apollo/client');
-    const { InMemoryCache } = require('@apollo/client/cache');
+  const { ApolloClient, HttpLink, ApolloProvider } = require('@apollo/client');
+  const { InMemoryCache } = require('@apollo/client/cache');
 
-    return { ApolloClient, HttpLink, ApolloProvider, InMemoryCache };
-  } catch {
-    /* eslint-disable node/no-extraneous-require */
-    const { ApolloProvider } = require('react-apollo');
-    const { ApolloClient } = require('apollo-client');
-    const { HttpLink } = require('apollo-link-http');
-    const { InMemoryCache } = require('apollo-cache-inmemory');
-    /* eslint-enable node/no-extraneous-require */
-
-    return { ApolloClient, HttpLink, ApolloProvider, InMemoryCache };
-  }
+  return { ApolloClient, HttpLink, ApolloProvider, InMemoryCache };
 })();
 
 function createTestClient() {
